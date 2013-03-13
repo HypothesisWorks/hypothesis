9092b7ec7171878b91084a44833a42236367c5cb
David R. MacIver <david@drmaciver.com>
2013-03-13 09:49:56 +0000

    nested test cases

--- hypothesis/tests/test_testdecorators.py
+++ hypothesis/tests/test_testdecorators.py
@@ -37,4 +37,17 @@ def test_reversing_preserves_integer_addition(xs):
 def test_reversing_does_not_preserve_integer_addition(xs):
     assert sum(xs) == sum(reversed(xs))
 
+class TestCases:
+    @given(int)
+    def test_abs_non_negative(self,x):
+        assert abs(x) >= 0
 
+    @fails
+    @given(int)
+    def test_int_is_always_negative(self,x):
+        assert x < 0
+
+    @fails
+    @given(float,float)
+    def test_float_addition_cancels(self,x,y):
+        assert x + (y - x) == y
