f2bbad705d3126539a8245dce0d0352e9a2c07e3
David R. MacIver <david@drmaciver.com>
2013-03-13 08:05:08 +0000

    decorators for tests

---
+++ hypothesis/testdecorators.py
@@ -0,0 +1,24 @@
+from functools import wraps
+from hypothesis.verifier import Verifier, Unfalsifiable
+
+def hypothesis(*generator_arguments,**kwargs):
+    try:
+        verifier = kwargs["verifier"]
+    except KeyError:
+        verifier = Verifier()
+
+    def run_test_with_generator(test):
+        @wraps(test)
+        def wrapped_test(*arguments):
+            # The only thing we accept in falsifying the test are assertion
+            # errors. Anything is a pass.
+            to_falsify = lambda *xs: test(*(arguments + xs)) or True
+            try:
+                falsifying_example = verifier.falsify(to_falsify, *generator_arguments)
+            except Unfalsifiable:
+                return
+           
+            # We run this one final time so we get good errors 
+            test(*(arguments + falsifying_example))
+        return wrapped_test
+    return run_test_with_generator
---
+++ hypothesis/tests/test_testdecorators.py
@@ -0,0 +1,21 @@
+from hypothesis.testannotations import hypothesis
+from functools import wraps
+
+import pytest
+
+def fails(f):
+    @wraps(f)
+    def inverted_test(*arguments,**kwargs):
+        with pytest.raises(AssertionError):
+            f(*arguments,**kwargs)
+    return inverted_test
+        
+
+@hypothesis(int,int)
+def test_int_addition_is_commutative(x,y):
+    assert x + y == y + x
+
+@fails
+@hypothesis(float,float,float)
+def test_float_addition_is_distributive(x,y,z):
+    assert x + (y + z) == (x + y) + z
