2a3d8c3913375240ebb572fbbf8fc14d7d3ff356
David R. MacIver <david@drmaciver.com>
2013-03-13 08:17:04 +0000

    more test cases for decorators. Correct module name

--- hypothesis/tests/test_testdecorators.py
+++ hypothesis/tests/test_testdecorators.py
@@ -1,4 +1,4 @@
-from hypothesis.testannotations import hypothesis
+from hypothesis.testdecorators import hypothesis
 from functools import wraps
 
 import pytest
@@ -10,12 +10,31 @@ def fails(f):
             f(*arguments,**kwargs)
     return inverted_test
         
-
 @hypothesis(int,int)
 def test_int_addition_is_commutative(x,y):
     assert x + y == y + x
 
 @fails
+@hypothesis(str,str)
+def test_str_addition_is_commutative(x,y):
+    assert x + y == y + x
+    
+@hypothesis(int,int,int)
+def test_int_addition_is_associative(x,y,z):
+    assert x + (y + z) == (x + y) + z
+
+@fails
 @hypothesis(float,float,float)
 def test_float_addition_is_associative(x,y,z):
     assert x + (y + z) == (x + y) + z
+
+@hypothesis([int])
+def test_reversing_preserves_integer_addition(xs):
+    assert sum(xs) == sum(reversed(xs))
+
+@fails
+@hypothesis([float])
+def test_reversing_does_not_preserve_integer_addition(xs):
+    assert sum(xs) == sum(reversed(xs))
+
+
