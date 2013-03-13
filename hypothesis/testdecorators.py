408583f38dcd4f0919389218fdf3dc7745b5b7a1
David R. MacIver <david@drmaciver.com>
2013-03-13 11:00:35 +0000

    better commenting of the logic here

--- hypothesis/testdecorators.py
+++ hypothesis/testdecorators.py
@@ -10,8 +10,8 @@ def given(*generator_arguments,**kwargs):
     def run_test_with_generator(test):
         @wraps(test)
         def wrapped_test(*arguments):
-            # The only thing we accept in falsifying the test are assertion
-            # errors. Anything is a pass.
+            # The only thing we accept in falsifying the test are exceptions 
+            # Returning successfully is always a pass.
             def to_falsify(*xs): 
                 try:
                     test(*(arguments + xs)) 
@@ -25,6 +25,8 @@ def given(*generator_arguments,**kwargs):
                 return
            
             # We run this one final time so we get good errors 
+            # Otherwise we would have swallowed all the reports of it actually
+            # having gone wrong.
             test(*(arguments + falsifying_example))
         return wrapped_test
     return run_test_with_generator
