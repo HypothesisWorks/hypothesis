ad37b0b2e298525542d1dbce12829be2d47c4aff
David R. MacIver <david@drmaciver.com>
2013-03-23 21:23:58 +0000

    missing file. Bah, rookie error. Closes issue #3

---
+++ hypothesis/flags.py
@@ -0,0 +1,24 @@
+class Flags(object):
+    def __init__(self, flags=None):
+        self.flags = frozenset(flags or {})
+
+    def __repr__(self):
+        return "Flags(%s)" % ', '.join(map(str,self.flags))
+
+    def enabled(self, flag):
+        return flag in self.flags
+
+    def with_enabled(self, *flags):
+        x = set(self.flags)
+        for f in flags:
+            x.add(f)
+        return Flags(x)
+
+    def with_disabled(self, *flags):
+        x = set(self.flags)
+        for f in flags:
+            x.remove(y)
+        return Flags(x)
+
+    def clone(self):
+        return Flags(self.flags)
