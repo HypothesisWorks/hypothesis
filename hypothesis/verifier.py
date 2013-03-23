fbe0c7e28bde339e2fa5a42b08453e24bce35e6f
David R. MacIver <david@drmaciver.com>
2013-03-23 19:08:50 +0000

    start using flags in the search strategy

--- hypothesis/verifier.py
+++ hypothesis/verifier.py
@@ -1,5 +1,8 @@
 from hypothesis.searchstrategy import SearchStrategy, SearchStrategies
+from hypothesis.flags import Flags
+
 from itertools import islice
+from random import random
 
 def assume(condition):
     if not condition: raise UnsatisfiedAssumption()
@@ -9,6 +12,7 @@ class Verifier:
                         starting_size = 1.0,
                         warming_rate = 0.5,
                         cooling_rate = 0.1,
+                        runs_to_explore_flags = 3,
                         max_size = 512,
                         max_failed_runs = 10):
         self.search_strategies = search_strategies or SearchStrategies()
@@ -16,11 +20,12 @@ class Verifier:
         self.warming_rate = warming_rate
         self.cooling_rate = cooling_rate
         self.max_size = max_size
-        self.max_failed_runs = max_failed_runs 
+        self.max_failed_runs = max_failed_runs
+        self.runs_to_explore_flags = runs_to_explore_flags 
                         
     def falsify(self, hypothesis, *argument_types):
         search_strategy = self.search_strategies.specification_for(argument_types)
-        flags = search_strategy.flags()
+        flags = None
 
         def falsifies(args):
             try:
@@ -39,9 +44,21 @@ class Verifier:
                 return x
 
         while temperature < self.max_size:
-            falsifying_example = look_for_a_falsifying_example(temperature)
+            rtf = self.runs_to_explore_flags
+            for i in xrange(self.runs_to_explore_flags):
+                # Try a number of degrees of turning flags on, spaced evenly but
+                # with the lowest probability of a flag being on being > 0 and 
+                # the highest < 1
+                # Note that as soon as we find a falsifying example with a set of
+                # flags, those are the flags we'll be using for the rest of the
+                # run
+                p = float(i + 1)/(rtf + 1)
+                flags = Flags([x for x in search_strategy.flags().flags if random() <= p])
+                falsifying_example = look_for_a_falsifying_example(temperature)
+                if falsifying_example:
+                    break
             if falsifying_example:
-                break
+                    break
             temperature += self.warming_rate
 
         if not falsifying_example: raise Unfalsifiable(hypothesis)
