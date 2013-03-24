4ed691e21661a922fbf30021be26aada6ec65674
David R. MacIver <david@drmaciver.com>
2013-03-24 13:47:49 +0000

    release 0.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info >= (3,):
 
 setup(
     name='hypothesis',
-    version='0.1.1',
+    version='0.1.2',
     author='David R. MacIver',
     author_email='david@drmaciver.com',
     packages=['hypothesis'],
