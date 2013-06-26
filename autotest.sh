bb373d7a95f0b70ae21b2cd390ee07ff4bda87ac
David R. MacIver <david@drmaciver.com>
2013-06-26 20:46:50 +0100

    run tests in order of most recently modified to least

--- autotest.sh
+++ autotest.sh
@@ -1,4 +1,8 @@
 SELF=$(readlink -f $0)
+py.test --maxfail=1 $(
+    find hypothesis -name "test_*.py"  -printf '%T@ %p\n' |
+    sort -k 1nr | 
+    sed 's/^[^ ]* //'
+)
 inotifywait -qe modify $(find hypothesis -name "*.py") $SELF pytest.ini
-py.test hypothesis --maxfail=1
 exec $SELF
