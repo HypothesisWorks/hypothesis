6b17df4a790b03dd86d2a1be60afd99db0d9355a
David R. MacIver <david@drmaciver.com>
2015-01-07 21:25:33 +0000

    fix the parameter value for a while, unless we're getting lots of failures to satisfy

--- hypothesis/verifier.py
+++ hypothesis/verifier.py
@@ -15,11 +15,16 @@ class Verifier(object):
                  min_satisfying_examples=5,
                  max_examples=100,
                  max_falsifying_examples=5,
+                 max_runs_per_parameter_value=10,
+                 max_unsatisfied_per_parameter_value=3,
                  timeout=60, random=None):
         self.search_strategies = search_strategies or SearchStrategies()
         self.min_satisfying_examples = min_satisfying_examples
         self.max_falsifying_examples = max_falsifying_examples
         self.max_examples = max_examples
+        self.max_runs_per_parameter_value = max_runs_per_parameter_value
+        self.max_unsatisfied_per_parameter_value = (
+            max_unsatisfied_per_parameter_value)
         self.timeout = timeout
         self.start_time = time.time()
         self.random = random or Random()
@@ -47,6 +52,9 @@ class Verifier(object):
         satisfying_examples = 0
         timed_out = False
 
+        pv = search_strategy.parameter.draw(self.random)
+        runs_with_value = 0
+        unsatisfied_with_value = 0
         while not (
             examples_found >= self.max_examples or
             len(falsifying_examples) >= self.max_falsifying_examples
@@ -54,14 +62,24 @@ class Verifier(object):
             if self.time_to_call_it_a_day():
                 timed_out = True
                 break
-            examples_found += 1
-            pv = search_strategy.parameter.draw(self.random)
+            if (
+                runs_with_value >= self.max_runs_per_parameter_value or
+                unsatisfied_with_value >=
+                self.max_unsatisfied_per_parameter_value
+            ):
+                pv = search_strategy.parameter.draw(self.random)
+                runs_with_value = 0
+                unsatisfied_with_value = 0
+            else:
+                runs_with_value += 1
             args = search_strategy.produce(self.random, pv)
+            examples_found += 1
             try:
                 is_falsifying_example = not hypothesis(*args)
             except AssertionError:
                 is_falsifying_example = True
             except UnsatisfiedAssumption:
+                unsatisfied_with_value += 1
                 continue
             satisfying_examples += 1
             if is_falsifying_example:
