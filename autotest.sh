11b528d8aec542c9121efb9be87b1160821582e6
David R. MacIver <david@drmaciver.com>
2015-01-09 16:44:05 +0000

    clean up htmlcov for consistency

--- autotest.sh
+++ autotest.sh
@@ -3,6 +3,8 @@ SELF=$(readlink -f $0)
 coverage run --branch --include 'hypothesis/*'\
     $(which py.test) -v hypothesis --capture=no 
 
+rm -rf htmlcov
+
 coverage html
 
 inotifywait -qe modify $(find hypothesis -name "*.py") $SELF pytest.ini
