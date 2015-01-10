80ff401279a249c73331208153cb7a8d0de1f9fc
David R. MacIver <david@drmaciver.com>
2015-01-10 12:04:34 +0000

    You see the lengths I go to for you people?

--- hypothesis/internal/utils/reflection.py
+++ hypothesis/internal/utils/reflection.py
@@ -1,5 +1,13 @@
+"""
+This file can approximately be considered the collection of hypothesis going to
+really unreasonable lengths to produce pretty output.
+"""
+
 import inspect
 from six.moves import xrange
+import types
+import ast
+import re
 
 
 def convert_keyword_arguments(function, args, kwargs):
@@ -47,3 +55,98 @@ def convert_keyword_arguments(function, args, kwargs):
                 function.__name__, bad_kwarg
             ))
     return tuple(new_args), kwargs
+
+
+def extract_all_lambdas(tree):
+    lambdas = []
+
+    class Visitor(ast.NodeVisitor):
+        def visit_Lambda(self, node):
+            lambdas.append(node)
+
+    Visitor().visit(tree)
+
+    return lambdas
+
+
+def args_for_lambda_ast(l):
+    return [n.id for n in l.args.args]
+
+
+def find_offset(string, line, column):
+    if line < 1:
+        raise ValueError("Line out of range: %d < 1" % (line,))
+    current_line = 1
+    current_line_offset = 0
+    while current_line < line:
+        current_line_offset = string.index("\n", current_line_offset+1)
+        current_line += 1
+    return current_line_offset + column
+
+
+WHITESPACE = re.compile('\s+')
+
+
+def extract_lambda_source(f):
+    """
+    Extracts a single lambda expression from the string source. Returns a
+    string indicating an unknown body if it gets confused in any way.
+
+    This is not a good function and I am sorry for it. Forgive me my sins, oh
+    lord
+    """
+    args = inspect.getargspec(f).args
+    if_confused = "lambda %s: <unknown>" % (', '.join(args),)
+    try:
+        source = inspect.getsource(f)
+    except IOError:
+        return if_confused
+
+    try:
+        tree = ast.parse(source)
+    except IndentationError:
+        source = "with 0:\n" + source
+        tree = ast.parse(source)
+
+    all_lambdas = extract_all_lambdas(tree)
+    aligned_lambdas = [
+        l for l in all_lambdas
+        if args_for_lambda_ast
+    ]
+    if len(aligned_lambdas) != 1:
+        return if_confused
+    lambda_ast = aligned_lambdas[0]
+    line_start = lambda_ast.lineno
+    column_offset = lambda_ast.col_offset
+    source = source[find_offset(source, line_start, column_offset):].strip()
+
+    source = source[source.index("lambda"):]
+
+    for i in xrange(len(source), -1, -1):
+        try:
+            ast.parse(source[:i])
+            source = source[:i]
+            break
+        except SyntaxError:
+            pass
+    source = WHITESPACE.sub(source, " ")
+    source = source.strip()
+    if source[0] == '(' and source[-1] == ')':
+        source = source[1:-1]
+    return source
+
+
+def get_pretty_function_description(f):
+    name = f.__name__
+    if name == '<lambda>':
+        return extract_lambda_source(f)
+    elif isinstance(f, types.MethodType):
+        self = f.im_self
+        if self is None:
+            return "%s.%s" % (f.im_class.__name__, name)
+        elif inspect.isclass(self):
+            return "%s.%s" % (self.__name__, name)
+        else:
+            return "%r.%s" % (self, name)
+    else:
+        return name
--- hypothesis/tests/test_reflection.py
+++ hypothesis/tests/test_reflection.py
@@ -1,10 +1,20 @@
-from hypothesis.internal.utils.reflection import convert_keyword_arguments
+from hypothesis.internal.utils.reflection import (
+    convert_keyword_arguments,
+    get_pretty_function_description,
+)
 import pytest
+
+
+def result(f, args, kwargs):
+    try:
+        return (0, f(*args, **kwargs))
+    except Exception as e:
+        return (1, e.__class__, e.args)
 
 
 def do_conversion_test(f, args, kwargs):
     cargs, ckwargs = convert_keyword_arguments(f, args, kwargs)
-    assert f(*args, **kwargs) == f(*cargs, **ckwargs)
+    assert result(f, args, kwargs) == result(f, cargs, ckwargs)
 
 
 def test_simple_conversion():
@@ -87,3 +97,56 @@ def test_errors_on_extra_kwargs():
     with pytest.raises(TypeError) as e2:
         convert_keyword_arguments(foo, (1,), {'b': 1, 'c': 2})
     assert 'keyword' in e2.value.args[0]
+
+
+def destructure_my_arguments((a, b), c):
+    return a, b, c
+
+
+def test_can_handle_destructuring_functions():
+    do_conversion_test(
+        destructure_my_arguments,
+        ((1, 2,),), {'c': 3})
+    do_conversion_test(
+        destructure_my_arguments,
+        ((1, 2,), 3), {})
+
+
+def test_names_of_functions_are_pretty():
+    assert get_pretty_function_description(
+        test_names_of_functions_are_pretty
+    ) == 'test_names_of_functions_are_pretty'
+
+
+class Foo(object):
+    @classmethod
+    def bar(cls):
+        pass  # pragma: no cover
+
+    def baz(cls):
+        pass  # pragma: no cover
+
+    def __repr__(self):
+        return "SoNotFoo()"
+
+
+def test_class_names_are_included_in_class_method_prettiness():
+    assert get_pretty_function_description(Foo.bar) == 'Foo.bar'
+
+
+def test_repr_is_included_in_bound_method_prettiness():
+    assert get_pretty_function_description(Foo().baz) == 'SoNotFoo().baz'
+
+
+def test_source_of_lambda_is_pretty():
+    assert get_pretty_function_description(lambda x: True) == 'lambda x: True'
+
+
+def test_variable_names_are_not_pretty():
+    t = lambda x: True
+    assert get_pretty_function_description(t) == 'lambda x: True'
+
+
+def test_does_not_error_on_dynamically_defined_functions():
+    x = eval('lambda t: 1')
+    get_pretty_function_description(x)
