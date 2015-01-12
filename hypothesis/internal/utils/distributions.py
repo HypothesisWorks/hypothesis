5446e9f155565ddff72e8c67fd65d1eeaa18807d
David R. MacIver <david@drmaciver.com>
2015-01-12 08:09:41 +0000

    a vague nod to documentation

--- hypothesis/internal/utils/distributions.py
+++ hypothesis/internal/utils/distributions.py
@@ -2,6 +2,10 @@ import math
 
 
 def geometric(random, p):
+    """
+    Generate a geometric integer in the range [0, infinity) with expected value
+    1 / p - 1
+    """
     denom = math.log1p(-p)
     return int(math.log(random.random()) / denom)
 
