71da10181e803e487fcb2276ea0a2c432b44b6fc
David R. MacIver <david@drmaciver.com>
2015-01-14 16:34:14 +0100

    Raise timeout in tests.
    This is because Travis builds are *really* slow, so tests are
    occasionally failing due to running out of time to minimize,
    and frankly I'm getting tired of it.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import hypothesis.settings
 import resource
 
 hypothesis.settings.default.max_examples = 1000
-hypothesis.settings.default.timeout = 15
+hypothesis.settings.default.timeout = 30
 
 MAX_MEMORY = 10
 
