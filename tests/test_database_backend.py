7db558257cb6ad966096d20406bd370ccae9fcc1
David R. MacIver <david@drmaciver.com>
2015-01-17 19:51:17 +0100

    Add a test specifically for the backend interface
    Right now this mostly recreates the logic of InMemoryBackend so isn't
    actually very useful. A better backend implementation to follow which
    will make it actually test something.

---
+++ tests/test_database_backend.py
@@ -0,0 +1,17 @@
+from hypothesis import given
+from hypothesis.database.backend import InMemoryBackend
+from hypothesis.internal.compat import text_type
+
+
+@given([(text_type, text_type)])
+def test_backend_returns_what_you_put_in(xs):
+    backend = InMemoryBackend()
+    mapping = {}
+    for key, value in xs:
+        mapping.setdefault(key, set()).add(value)
+        backend.save(key, value)
+    for key, values in mapping.items():
+        backend_contents = list(backend.fetch(key))
+        distinct_backend_contents = set(backend_contents)
+        assert len(backend_contents) == len(distinct_backend_contents)
+        assert distinct_backend_contents == set(values)
