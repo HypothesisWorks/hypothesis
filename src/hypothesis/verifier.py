9a6e26aaf67f86ae035110c65e099c68e7ef4798
David R. MacIver <david@drmaciver.com>
2015-01-18 10:29:24 +0100

    Make verifier.py pylint clean

--- src/hypothesis/verifier.py
+++ src/hypothesis/verifier.py
@@ -15,16 +15,24 @@ from hypothesis.internal.tracker import Tracker
 
 
 def assume(condition):
+    """
+    Assert a precondition for this test. If this is not truthy then the
+    test will abort but not fail and Hypothesis will make a "best effort"
+    attempt to avoid similar examples in future.
+    """
     if not condition:
         raise UnsatisfiedAssumption()
 
 
-class Verifier(object):
+class Verifier(object):  # pylint: disable=too-few-public-methods
+    """
+    A wrapper object holding state required for a falsify invocation.
+    """
     def __init__(
-        self,
-        strategy_table=None,
-        random=None,
-        settings=None,
+            self,
+            strategy_table=None,
+            random=None,
+            settings=None,
     ):
         if settings is None:
             settings = hs.default
@@ -44,17 +52,23 @@ class Verifier(object):
             self.random = random or Random()
         self.max_regenerations = 0
 
-    def falsify(self, hypothesis, *argument_types):
+    def falsify(
+            self, hypothesis, *argument_types
+    ):  # pylint: disable=too-many-locals,too-many-branches
+        """
+        Attempt to construct an example tuple x matching argument_types such
+        that hypothesis(*x) returns a falsey value or throws an AssertionError
+        """
         random = self.random
         if random is None:
             random = Random(
                 function_digest(hypothesis)
             )
 
-        search_strategy = (self.strategy_table
-                               .specification_for(argument_types))
+        search_strategy = (
+            self.strategy_table.specification_for(argument_types))
 
-        def falsifies(args):
+        def falsifies(args):  # pylint: disable=missing-docstring
             try:
                 return not hypothesis(*search_strategy.copy(args))
             except AssertionError:
@@ -75,13 +89,11 @@ class Verifier(object):
             min_satisfying_examples = 1
             parameter_values = 1
 
-        def generate_parameter_values():
-            return [
-                search_strategy.parameter.draw(random)
-                for _ in xrange(parameter_values)
-            ]
+        parameter_values = [
+            search_strategy.parameter.draw(random)
+            for _ in hrange(parameter_values)
+        ]
 
-        parameter_values = generate_parameter_values()
         accepted_examples = [0] * max_examples
         rejected_examples = [0] * max_examples
         track_seen = Tracker()
@@ -89,14 +101,15 @@ class Verifier(object):
         start_time = time.time()
 
         def time_to_call_it_a_day():
+            """Have we exceeded our timeout?"""
             return time.time() >= start_time + self.timeout
 
         initial_run = 0
         skipped_examples = 0
 
         while not (
-            examples_found >= max_examples or
-            len(falsifying_examples) >= 1
+                examples_found >= max_examples or
+                len(falsifying_examples) >= 1
         ):
             if time_to_call_it_a_day():
                 break
@@ -111,9 +124,9 @@ class Verifier(object):
                         accepted_examples[k] + 1, rejected_examples[k] + 1
                     )
                 )
-            pv = parameter_values[i]
+            parameter = parameter_values[i]
 
-            args = search_strategy.produce(random, pv)
+            args = search_strategy.produce(random, parameter)
             assert search_strategy.could_have_produced(args)
 
             if track_seen.track(args) > 1:
@@ -132,6 +145,7 @@ class Verifier(object):
                 skipped_examples = 0
             examples_found += 1
             try:
+                # pylint: disable=star-args
                 is_falsifying_example = not hypothesis(
                     *search_strategy.copy(args))
             except AssertionError:
@@ -154,14 +168,16 @@ class Verifier(object):
             else:
                 raise Unfalsifiable(hypothesis)
 
-        for x in falsifying_examples:
-            if not falsifies(x):
-                raise Flaky(hypothesis, x)
+        for example in falsifying_examples:
+            if not falsifies(example):
+                raise Flaky(hypothesis, example)
 
         best_example = falsifying_examples[0]
 
-        for t in search_strategy.simplify_such_that(best_example, falsifies):
-            best_example = t
+        for simpler in search_strategy.simplify_such_that(
+                best_example, falsifies
+        ):
+            best_example = simpler
             if time_to_call_it_a_day():
                 break
 
@@ -169,20 +185,35 @@ class Verifier(object):
 
 
 def falsify(*args, **kwargs):
+    """
+    A convenience wrapper function for Verifier.falsify
+    """
     return Verifier(**kwargs).falsify(*args)
 
 
 class HypothesisException(Exception):
+    """
+    Generic parent class for exceptions thrown by Hypothesis
+    """
     pass
 
 
 class UnsatisfiedAssumption(HypothesisException):
+    """
+    An internal error raised by assume. If you're seeing this error something
+    has gone wrong.
+    """
 
     def __init__(self):
         super(UnsatisfiedAssumption, self).__init__('Unsatisfied assumption')
 
 
 class Unfalsifiable(HypothesisException):
+    """
+    The hypothesis we have been asked to falsify appears to be always true.
+    This does not guarantee that no counter-example exists, only that we
+    were unable to find one.
+    """
 
     def __init__(self, hypothesis, extra=''):
         super(Unfalsifiable, self).__init__(
@@ -192,6 +223,12 @@ class Unfalsifiable(HypothesisException):
 
 
 class Exhausted(Unfalsifiable):
+    """
+    We appear to have considered the entire example space available before we
+    ran out of time or number of examples. This does not guarantee that we have
+    considered the whole example space (it could just be a bad search strategy)
+    but it makes it pretty likely that this hypothesis is just always true.
+    """
     def __init__(self, hypothesis, n_examples):
         super(Exhausted, self).__init__(
             hypothesis, " exhausted parameter space after %d examples" % (
@@ -201,16 +238,42 @@ class Exhausted(Unfalsifiable):
 
 
 class Unsatisfiable(HypothesisException):
+    """
+    We ran out of time or examples before we could find enough examples which
+    satisfy the assumptions of this hypothesis. This could be because the
+    function is too slow. If so, try upping the timeout. It could also be
+    because the function is using assume in a way that is too hard to satisfy.
+    If so, try writing a custom strategy or using a better starting point (e.g
+    if you are requiring a list has unique values you could instead filter out
+    all duplicate values from the list)
+    """
 
     def __init__(self, hypothesis, examples, run_time):
-        super(Unsatisfiable, self).__init__(
-            ('Unable to satisfy assumptions of hypothesis %s. ' +
-             'Only %s examples found after %g seconds') % (
-                get_pretty_function_description(hypothesis), str(examples),
+        super(Unsatisfiable, self).__init__((
+            'Unable to satisfy assumptions of hypothesis %s. ' +
+            'Only %s examples found after %g seconds'
+            ) % (
+                get_pretty_function_description(hypothesis),
+                str(examples),
                 run_time))
 
 
 class Flaky(HypothesisException):
+    """
+    This function appears to fail non-deterministically: We have seen it fail
+    when passed this example at least once, but a subsequent invocation did not
+    fail.
+
+    Common causes for this problem are:
+        1. The function depends on external state. e.g. it uses an external
+           random number generator. Try to make a version that passes all the
+           relevant state in from Hypothesis.
+        2. The function is suffering from too much recursion and its failure
+           depends sensitively on where it's been called from.
+        3. The function is timing sensitive and can fail or pass depending on
+           how long it takes. Try breaking it up into smaller functions which
+           dont' do that and testing those instead.
+    """
     def __init__(self, hypothesis, example):
         super(Flaky, self).__init__((
             "Hypothesis %r produces unreliable results: %r falsified it on the"
@@ -219,7 +282,10 @@ class Flaky(HypothesisException):
 
 
 class Timeout(Unfalsifiable):
-
+    """
+    We were unable to find enough examples that satisfied the preconditions of
+    this hypothesis in the amount of time allotted to us.
+    """
     def __init__(self, hypothesis, satisfying_examples, run_time):
         super(Timeout, self).__init__(
             hypothesis,
