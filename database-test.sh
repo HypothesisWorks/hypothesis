b09e040cc50c8b09745f45fffaa67c17a28cc0f1
David R. MacIver <david@drmaciver.com>
2015-01-20 13:40:47 +0100

    Trace database test.

--- database-test.sh
+++ database-test.sh
@@ -1,3 +1,5 @@
+set -e -o xtrace
+
 export HYPOTHESIS_DATABASE_FILE=$(mktemp --suffix=.db)
 HYPOTHESIS_DATABASE_FILE=hypothesis.db PYTHONPATH=src python -u -m coverage run -a --branch --include 'src/hypothesis/*' --omit 'src/hypothesis/settings.py,src/hypothesis/internal/compat.py' $(which py.test) -v tests --ignore=tests/test_recursively.py
 PYTHONPATH=src python -c '
