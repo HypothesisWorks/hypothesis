e409021f918be679031979aa97c21f0a2d9a16ee
David R. MacIver <david@drmaciver.com>
2015-01-20 16:27:03 +0100

    Print out the database file to check it's right

--- database-test.sh
+++ database-test.sh
@@ -1,5 +1,6 @@
 set -e -o xtrace
 
+export PYTHONDONTWRITEBYTECODE=x
 export HYPOTHESIS_DATABASE_FILE=$(mktemp --suffix=.db)
 HYPOTHESIS_DATABASE_FILE=hypothesis.db PYTHONPATH=src python -u -m coverage run -a --branch --include 'src/hypothesis/*' --omit 'src/hypothesis/settings.py,src/hypothesis/internal/compat.py' $(which py.test) -v tests --ignore=tests/test_recursively.py
 PYTHONPATH=src python -c '
@@ -9,9 +10,11 @@ from hypothesis.database import ExampleDatabase
 from hypothesis.database.backend import SQLiteBackend
 import os
 import sys
+database_file = os.getenv("HYPOTHESIS_DATABASE_FILE")
+print("Database file is", database_file)
 
 db = ExampleDatabase(
-    backend=SQLiteBackend(os.getenv("HYPOTHESIS_DATABASE_FILE")))
+    backend=SQLiteBackend(database_file))
 data = list(db.storage_for((((int,), {}),)).fetch())
 if not data:
     print("No integer examples in database")
