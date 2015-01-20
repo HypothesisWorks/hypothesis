a03fb3ac7357f9d379468b0be7626ddf0e14e9f9
David R. MacIver <david@drmaciver.com>
2015-01-20 08:25:09 +0100

    Better selection of new parameters
    Idea: We track whether the last parameter was new, and thus we track
    the number of times a new parameter has produced a good result. This
    lets the "generate a new parameter and pick from it" arm participate
    in the bandit like any other, so that we pick it when it seems like
    the best bet based on prior evidence.

--- src/hypothesis/examplesource.py
+++ src/hypothesis/examplesource.py
@@ -31,6 +31,9 @@ class ExampleSource(object):
         self.min_parameters = min_parameters
         self.bad_counts = []
         self.counts = []
+        self.is_new_parameter = True
+        self.new_parameter_bad = 0
+        self.mark_set = False
 
     def mark_bad(self):
         """The last example was bad.
@@ -38,22 +41,32 @@ class ExampleSource(object):
         If possible can we have less of that please?
 
         """
+        if self.mark_set:
+            raise ValueError('This parameter has already been marked')
+        self.mark_set = True
         if self.last_parameter_index < 0:
             return
         self.bad_counts[self.last_parameter_index] += 1
+        if self.is_new_parameter:
+            self.new_parameter_bad += 1
 
     def new_parameter(self):
         result = self.strategy.parameter.draw(self.random)
         self.parameters.append(result)
         self.bad_counts.append(0)
         self.counts.append(1)
+        self.is_new_parameter = True
         return result
 
     def pick_a_parameter(self):
+        self.mark_set = False
         if len(self.parameters) < self.min_parameters:
             return self.new_parameter()
         else:
-            best_score = self.random.random()
+            best_score = self.random.betavariate(
+                len(self.parameters) - self.new_parameter_bad + 1,
+                self.new_parameter_bad + 1,
+            )
             best_index = -1
 
             for i in hrange(len(self.parameters)):
@@ -70,7 +83,7 @@ class ExampleSource(object):
             if best_index < 0:
                 self.last_parameter_index = len(self.parameters)
                 return self.new_parameter()
-
+            self.is_new_parameter = False
             self.last_parameter_index = best_index
             self.counts[self.last_parameter_index] += 1
             return self.parameters[self.last_parameter_index]
