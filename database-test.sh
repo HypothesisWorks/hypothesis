c41d39fa85e8a0a099289d22f5eb3a68f4c57ae6
David R. MacIver <david@drmaciver.com>
2015-01-20 17:15:26 +0100

    Use the correct database file.

--- database-test.sh
+++ database-test.sh
@@ -2,7 +2,7 @@ set -e -o xtrace
 
 export PYTHONDONTWRITEBYTECODE=x
 export HYPOTHESIS_DATABASE_FILE=$(mktemp --suffix=.db)
-HYPOTHESIS_DATABASE_FILE=hypothesis.db PYTHONPATH=src python -u -m coverage run -a --branch --include 'src/hypothesis/*' --omit 'src/hypothesis/settings.py,src/hypothesis/internal/compat.py' $(which py.test) -v tests --ignore=tests/test_recursively.py
+PYTHONPATH=src python -u -m coverage run -a --branch --include 'src/hypothesis/*' --omit 'src/hypothesis/settings.py,src/hypothesis/internal/compat.py' $(which py.test) -v tests --ignore=tests/test_recursively.py
 PYTHONPATH=src python -c '
 from __future__ import print_function
 
@@ -20,4 +20,4 @@ if not data:
     print("No integer examples in database")
     sys.exit(1)
 '
-HYPOTHESIS_DATABASE_FILE=hypothesis.db PYTHONPATH=src python -u -m coverage run -a --branch --include 'src/hypothesis/*' --omit 'src/hypothesis/settings.py,src/hypothesis/internal/compat.py' $(which py.test) -v tests --ignore=tests/test_recursively.py
+PYTHONPATH=src python -u -m coverage run -a --branch --include 'src/hypothesis/*' --omit 'src/hypothesis/settings.py,src/hypothesis/internal/compat.py' $(which py.test) -v tests --ignore=tests/test_recursively.py
