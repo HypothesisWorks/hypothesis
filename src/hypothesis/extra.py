4e2f88f086c58a6d8b53f25afcec45200b08e871
David R. MacIver <david@drmaciver.com>
2015-02-04 14:26:22 +0100

    Simplify load_entry_points and mark for coverage

--- src/hypothesis/extra.py
+++ src/hypothesis/extra.py
@@ -1,14 +1,8 @@
 import pkg_resources
 
-entry_points_loaded = False
-
 
 def load_entry_points():
-    global entry_points_loaded
-    if entry_points_loaded:
-        return
-    entry_points_loaded = True
     for entry_point in pkg_resources.iter_entry_points(
         group='hypothesis.extra'
     ):
-        entry_point.load()()
+        entry_point.load()()  # pragma: no cover
