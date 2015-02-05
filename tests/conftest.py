cbe108f80d93f13d2d40ee529c8551ea22c01d1b
David R. MacIver <david@drmaciver.com>
2015-02-05 22:08:06 +0100

    The resource module is not available on windows

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 import hypothesis.settings
-import resource
 
 hypothesis.settings.default.max_examples = 1000
 hypothesis.settings.default.timeout = -1
 
-MAX_MEMORY = 10
-
-resource.setrlimit(resource.RLIMIT_DATA, (MAX_MEMORY, MAX_MEMORY))
+try:
+    import resource
+    MAX_MEMORY = 10
+    resource.setrlimit(resource.RLIMIT_DATA, (MAX_MEMORY, MAX_MEMORY))
+except ImportError:
+    pass
