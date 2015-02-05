5da024aefd4ac168555dd3894b838180ebf90471
David R. MacIver <david@drmaciver.com>
2015-02-05 19:15:32 +0100

    Only generate characters in range [0, maxunicode]

--- src/hypothesis/internal/compat.py
+++ src/hypothesis/internal/compat.py
@@ -1,6 +1,5 @@
 # pylint: skip-file
 import sys
-import struct
 
 PY3 = sys.version_info[0] == 3
 
@@ -17,9 +16,4 @@ else:
     from __builtin__ import xrange as hrange
     ARG_NAME_ATTRIBUTE = 'id'
     integer_types = (int, long)
-
-    def hunichr(i):
-        try:
-            return unichr(i)
-        except ValueError:
-            return struct.pack('i', i).decode('utf-32')
+    hunichr = unichr
