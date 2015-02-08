d7b6cbb1010e6899c66eda565dac3d8bdea56b4a
David R. MacIver <david@drmaciver.com>
2015-02-08 17:47:59 +0100

    pyformat pass

--- tests/test_eval_as_source.py
+++ tests/test_eval_as_source.py
@@ -2,12 +2,12 @@ from hypothesis.internal.utils.reflection import source_exec_as_module
 
 
 def test_can_eval_as_source():
-    assert source_exec_as_module("foo=1").foo == 1
+    assert source_exec_as_module('foo=1').foo == 1
 
 
 def test_caches():
-    x = source_exec_as_module("foo=2")
-    y = source_exec_as_module("foo=2")
+    x = source_exec_as_module('foo=2')
+    y = source_exec_as_module('foo=2')
     assert x is y
 
 
@@ -18,6 +18,7 @@ def test_recurse():
     assert not (
         source_exec_as_module("too_much_recursion = False").too_much_recursion)
 """
+
 
 def test_can_call_self_recursively():
     source_exec_as_module(RECURSIVE).test_recurse()
