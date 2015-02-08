9714985f34ebd46fce9a8021baa0f3f99c0bc88f
David R. MacIver <david@drmaciver.com>
2015-02-08 22:46:40 +0100

    also enforce division

--- src/hypothesis/extra.py
+++ src/hypothesis/extra.py
@@ -4,7 +4,7 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at http://mozilla.org/MPL/2.0/.
 
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, division
 
 # END HEADER
 
