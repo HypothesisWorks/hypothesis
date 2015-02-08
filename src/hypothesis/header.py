90267ea86219d2017c66e7219b7e5069e5f66ab1
David R. MacIver <david@drmaciver.com>
2015-02-08 20:49:38 +0100

    actually commit the header

---
+++ src/hypothesis/header.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import print_function, unicode_literals
+
+# END HEADER
