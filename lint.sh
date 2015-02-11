f740677234ac3069664500bbf8d07e6f21b6787b
David R. MacIver <david@drmaciver.com>
2015-02-11 09:58:22 +0100

    don't know how that ever worked

--- lint.sh
+++ lint.sh
@@ -9,5 +9,7 @@ isort -p hypothesis -ls -m 2 -w 75 \
 find hypothesis-extra/*/{src,tests} src tests -name '*.py' | xargs pyformat -i
 git diff --exit-code
 flake8 src tests --exclude=compat.py,test_reflection.py --ignore=E731
-rst-lint README.rst hypothesis-extra/*/README.rst
+for f in ./README.rst ./hypothesis-extra/*/README.rst; do
+  rst-lint $f
+done
 
