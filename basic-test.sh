669571607b3d8a7ee84339c2d8643737e0995449
David R. MacIver <david@drmaciver.com>
2015-02-21 12:40:26 +0100

    also run  manage.py in basic tests

--- basic-test.sh
+++ basic-test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e -o xtrace
 
-pip install hypothesis-extra/*/
+pip install --upgrade hypothesis-extra/*/
 python -m pytest --capture=no --strict tests/ hypothesis-extra/*/tests/
+for f in hypothesis-extra/*/manage.py; do
+    d=$(dirname $f)
+    pushd $d
+        python manage.py test
+    popd
+done
