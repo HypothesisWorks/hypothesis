da6c35fee545b1a0393e3cd2cb478861e300f8b1
David R. MacIver <david@drmaciver.com>
2015-02-21 13:08:02 +0100

    don't specify reruns in pytest.ini because it interferes with extras tests

--- basic-test.sh
+++ basic-test.sh
@@ -2,7 +2,8 @@
 set -e -o xtrace
 
 pip install --upgrade hypothesis-extra/*/
-python -m pytest --capture=no --strict tests/ hypothesis-extra/*/tests/
+python -m pytest --capture=no --strict --reruns=1 tests/
+python -m pytest --capture=no --strict hypothesis-extra/*/tests/
 for f in hypothesis-extra/*/manage.py; do
     d=$(dirname $f)
     pushd $d
