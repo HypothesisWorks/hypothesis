9be9d06d021ef1ed871b8459060d4da900c10ef2
David R. MacIver <david@drmaciver.com>
2015-03-19 15:15:36 +0100

    fix up the database test. Also run it on pypy

--- database-test.sh
+++ database-test.sh
@@ -5,9 +5,10 @@ set -o xtrace -e
 export PYTHONDONTWRITEBYTECODE=x
 export HYPOTHESIS_DATABASE_FILE=$(python -c 'import tempfile; print(tempfile.mkstemp(suffix=".db")[-1])')
 rm $HYPOTHESIS_DATABASE_FILE
-python -u -m pytest -v tests/
+python -u -m pytest -v tests/test_testdecorators.py
 python -c '
-from __future__ import print_function
+from __future__ import division, print_function, absolute_import, \
+    unicode_literals
 
 from hypothesis.database import ExampleDatabase
 from hypothesis.database.backend import SQLiteBackend
@@ -18,9 +19,10 @@ print("Database file is", database_file)
 
 db = ExampleDatabase(
     backend=SQLiteBackend(database_file))
-data = list(db.storage_for(((int,), {})).fetch())
+data = list(db.storage_for((((), {"x": int}),)).fetch())
+data += list(db.storage_for((((), {b"x": int}),)).fetch())
 if not data:
     print("No integer examples in database")
     sys.exit(1)
 '
-python -u -m pytest -v tests/
+python -u -m pytest -v tests/test_testdecorators.py
