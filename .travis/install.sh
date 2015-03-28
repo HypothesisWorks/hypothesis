15da75a87131cc980673e5cf084e7e06be105a07
David R. MacIver <david@drmaciver.com>
2015-03-28 17:44:24 +0100

    Use pyenv to manage python dependencies everywhere
    Previously it was only used for OSX builds. This turns it on for
    Linux builds too.
    
    The original reason for this was to be able to test all versions of
    Python supported by pyenv on every platform (I previously didn't have
    access to pypy3 on Linux).
    
    However this enabled a bunch of cool things: Because all my python
    installs are local now, I no longer need sudo. *this* means that I
    can use the new Travis container based infrastructure, and this means
    that I can turn on caching!
    
    (Turning on caching is actually non-optional for this. On Linux pyenv
    builds from source which takes a horrendous amount of time to do it on
    each build. By using caching we only have to pay that cost once).
    
    In particular by caching the ~/.cache/pip directory the build becomes
    much more robust against pip failures because we can always install
    from cache (I haven't tested this in anger so it may be that I need
    to do something to make this work a bit better, but it paves the way
    for it being possible)
    
    As a side-benefit, no longer doing apt-get update means my build can
    no longer be made to fail by rabbitmq.com being slow to respond and
    causing a timeout.
    
    This commit also removes rst-lint. It wasn't proving very useful and
    violates my "don't depend on things that don't work on Python 3"
    policy. As a result this commit also upgrades the version the lint
    process runs on to a modern version of Python. This has little
    effect except to make me feel better.

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,104 +3,63 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    DARWIN=true
-else
-    DARWIN=false
+if [ ! -e "$HOME/.pyenv/bin/pyenv" ] ; then
+  echo "pyenv does not exist"
+  if [ -e "$HOME/.pyenv" ] ; then
+    echo "Looks like a bad pyenv install. Deleting"
+    rm -rf $HOME/.pyenv
+  fi
 fi
 
-if [[ "$DARWIN" = true ]]; then
-    brew update
+$(dirname $0)/pyenv-installer
 
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
+export PATH="$HOME/.pyenv/bin:$HOME/.pyenv/shims:$PATH"
 
-    case "${TOXENV}" in
-        py26)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
-            ;;
-        py27)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
-            ;;
-        py32)
-            brew upgrade pyenv
-            pyenv install 3.2.6
-            pyenv global 3.2.6
-            ;;
-        py33)
-            brew upgrade pyenv
-            pyenv install 3.3.6
-            pyenv global 3.3.6
-            ;;
-        py34)
-            brew upgrade pyenv
-            pyenv install 3.4.2
-            pyenv global 3.4.2
-            ;;
-        pypy)
-            brew upgrade pyenv
-            pyenv install pypy-2.5.0
-            pyenv global pypy-2.5.0
-            ;;
-        pypy3)
-            brew upgrade pyenv
-            pyenv install pypy3-2.4.0
-            pyenv global pypy3-2.4.0
-            ;;
-        docs)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            sudo python get-pip.py
-            ;;
-    esac
-    pyenv rehash
 
-else
-    sudo add-apt-repository -y ppa:fkrull/deadsnakes
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
-    if [[ "${TOXENV}" == "pypy" ]]; then
-        sudo add-apt-repository -y ppa:pypy/ppa
-    fi
+pyenv update || echo "Update failed to complete. Ignoring"
 
-    if [[ "${OPENSSL}" == "0.9.8" ]]; then
-        sudo add-apt-repository -y "deb http://archive.ubuntu.com/ubuntu/ lucid main"
-    fi
 
-    sudo apt-get -y update
+case "${TOXENV}" in
+    py27)
+        PYVERSION=2.7.8
+        ;;
+    py32)
+        PYVERSION=3.2.6
+        ;;
+    py33)
+        PYVERSION=3.3.6
+        ;;
+    py34)
+        PYVERSION=3.4.2
+        ;;
+    pypy)
+        PYVERSION=pypy-2.5.0
+        ;;
+    pypy3)
+        PYVERSION=pypy3-2.4.0
+        ;;
+esac
 
-    if [[ "${OPENSSL}" == "0.9.8" ]]; then
-        sudo apt-get install -y --force-yes libssl-dev/lucid
-    fi
-
-    case "${TOXENV}" in
-        py26)
-            sudo apt-get install python2.6 python2.6-dev
-            ;;
-        py32)
-            sudo apt-get install python3.2 python3.2-dev
-            ;;
-        py33)
-            sudo apt-get install python3.3 python3.3-dev
-            ;;
-        py34)
-            sudo apt-get install python3.4 python3.4-dev
-            ;;
-        py3pep8)
-            sudo apt-get install python3.3 python3.3-dev
-            ;;
-        pypy)
-            sudo apt-get install --force-yes pypy pypy-dev
-            ;;
-        docs)
-            sudo apt-get install libenchant-dev
-            ;;
-    esac
+if [ -z "$PYVERSION" ]; then
+  PYVERSION=3.4.2
 fi
 
-sudo -H pip install virtualenv
-virtualenv ~/.venv
-source ~/.venv/bin/activate
+pyenv install -s $PYVERSION
+pyenv rehash
+pyenv global $PYVERSION
+pyenv local $PYVERSION
+
+python --version
 pip install --upgrade pip
-pip install tox
+pip install --upgrade virtualenv
+
+if [ ! -d "$HOME/.venv" ] ; then
+  rm -rf "$HOME/.venv"
+  virtualenv "$HOME/.venv"
+fi
+
+source $HOME/.venv/bin/activate
+pip install --upgrade tox
