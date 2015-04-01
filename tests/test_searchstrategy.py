9ce2ba5076b6ef24bf01ec6fa5886c189f58b269
David R. MacIver <david@drmaciver.com>
2015-04-01 10:25:46 +0200

    Resurrect the actually useful subset of test_searchstrategy.py

---
+++ tests/test_searchstrategy.py
@@ -0,0 +1,137 @@
+# coding=utf-8
+
+# Copyright (C) 2013-2015 David R. MacIver (david@drmaciver.com)
+
+# This file is part of Hypothesis (https://github.com/DRMacIver/hypothesis)
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+# END HEADER
+
+from __future__ import division, print_function, absolute_import, \
+    unicode_literals
+
+import random
+from collections import namedtuple
+
+import pytest
+import hypothesis.specifiers as specifiers
+from hypothesis.types import RandomWithSeed
+from hypothesis.internal.compat import hrange, text_type
+from hypothesis.searchstrategy.numbers import BoundedIntStrategy, \
+    RandomGeometricIntStrategy
+from hypothesis.searchstrategy.strategies import BuildContext, \
+    OneOfStrategy, strategy, one_of_strategies
+
+
+def test_or_errors_when_given_non_strategy():
+    bools = strategy((bool,))
+    with pytest.raises(ValueError):
+        bools | 'foo'
+
+
+def test_joining_zero_strategies_fails():
+    with pytest.raises(ValueError):
+        one_of_strategies(())
+
+
+def test_directly_joining_one_strategy_also_fails():
+    with pytest.raises(ValueError):
+        OneOfStrategy([RandomGeometricIntStrategy()])
+
+SomeNamedTuple = namedtuple('SomeNamedTuple', ('a', 'b'))
+
+
+def test_strategy_for_integer_range_produces_only_integers_in_that_range():
+    just_one_integer = strategy(specifiers.IntegerRange(1, 1))
+    for _ in hrange(100):
+        pv = just_one_integer.draw_parameter(random)
+        x = just_one_integer.produce_template(BuildContext(random), pv)
+        assert x == 1
+    some_integers = strategy(specifiers.IntegerRange(1, 10))
+    for _ in hrange(100):
+        pv = some_integers.produce_parameter(random)
+        x = some_integers.produce_template(BuildContext(random), pv)
+        assert 1 <= x <= 10
+
+
+def test_strategy_for_integer_range_can_produce_end_points():
+    some_integers = strategy(specifiers.IntegerRange(1, 10))
+    found = set()
+    for _ in hrange(1000):  # pragma: no branch
+        pv = some_integers.produce_parameter(random)
+        x = some_integers.produce_template(BuildContext(random), pv)
+        found.add(x)
+        if 1 in found and 10 in found:
+            break
+    else:
+        assert False  # pragma: no cover
+    assert 1 in found
+    assert 10 in found
+
+
+def last(xs):
+    t = None
+    for x in xs:
+        t = x
+    return t
+
+
+def test_simplify_integer_range_can_push_to_near_boundaries():
+    some_integers = strategy(specifiers.IntegerRange(1, 10))
+
+    predicates = [
+        (lambda x: True, 1),
+        (lambda x: x > 1, 2),
+        (lambda x: x > 5, 10),
+        (lambda x: x > 5 and x < 10, 9),
+    ]
+
+    for p, v in predicates:
+        some = False
+        for i in hrange(1, 10):
+            if p(i):
+                some = True
+                assert last(some_integers.simplify_such_that(i, p)) == v
+        assert some
+
+
+def test_rejects_invalid_ranges():
+    with pytest.raises(ValueError):
+        BoundedIntStrategy(10, 9)
+
+
+def test_random_repr_has_seed():
+    strat = strategy(random.Random)
+    rnd = strat.reify(
+        strat.produce_template(BuildContext(random.Random()), None))
+    seed = rnd.seed
+    assert text_type(seed) in repr(rnd)
+
+
+def test_random_only_produces_special_random():
+    st = strategy(random.Random)
+    assert isinstance(st.example(), RandomWithSeed)
+
+
+def test_just_strategy_uses_repr():
+    class WeirdRepr(object):
+
+        def __repr__(self):
+            return 'ABCDEFG'
+
+    assert repr(
+        strategy(specifiers.just(WeirdRepr()))
+    ) == 'JustStrategy(value=%r)' % (WeirdRepr(),)
+
+
+def test_can_map():
+    s = strategy(int).map(pack=lambda t: 'foo')
+    assert s.example() == 'foo'
+
+
+def test_sample_from_empty_errors():
+    with pytest.raises(ValueError):
+        strategy(specifiers.sampled_from([]))
