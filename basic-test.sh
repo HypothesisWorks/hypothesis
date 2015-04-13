4f66083908b1fbf6445da0b7009c45224b22e292
David R. MacIver <david@drmaciver.com>
2015-04-13 10:19:09 +0200

    Be explicit about installation order for basic tests

--- basic-test.sh
+++ basic-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e -o xtrace
 
-pip install --upgrade hypothesis-extra/*/
+pip install --upgrade hypothesis-extra/hypothesis-datetime/
+pip install --upgrade hypothesis-extra/hypothesis-fakefactory/
+pip install --upgrade hypothesis-extra/hypothesis-pytest/
+pip install --upgrade hypothesis-extra/hypothesis-django/
 python -m pytest --capture=no --strict tests/
 python -m pytest --capture=no --strict hypothesis-extra/*/tests/
 for f in hypothesis-extra/*/manage.py; do
