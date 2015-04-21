d2f976c98521594bb554f47c7d0e0102cb9cc8fd
David R. MacIver <david@drmaciver.com>
2015-04-21 17:30:33 +0200

    Print durations after tests have run

--- basic-test.sh
+++ basic-test.sh
@@ -5,8 +5,9 @@ pip install --upgrade hypothesis-extra/hypothesis-datetime/
 pip install --upgrade hypothesis-extra/hypothesis-fakefactory/
 pip install --upgrade hypothesis-extra/hypothesis-pytest/
 pip install --upgrade hypothesis-extra/hypothesis-django/
-python -m pytest --capture=no --strict tests/
-python -m pytest --capture=no --strict hypothesis-extra/*/tests/
+python -m pytest --capture=no --strict tests/ --durations=20
+python -m pytest --capture=no --strict hypothesis-extra/*/tests/ --durations=20
+
 for f in hypothesis-extra/*/manage.py; do
     d=$(dirname $f)
     pushd $d
