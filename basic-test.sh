eee2c402fa96c31e9c1e0fd507efbde52bf19685
David R. MacIver <david@drmaciver.com>
2015-04-22 22:55:39 +0200

    Back out pypy + numpy for now

--- basic-test.sh
+++ basic-test.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 set -e -o xtrace
 
-if [ "$(python -c 'import platform; print(platform.python_implementation())')" = "PyPy" ]; then
-    pip install git+https://bitbucket.org/pypy/numpy.git
-fi
-
-pip install --upgrade hypothesis-extra/hypothesis-datetime/
-pip install --upgrade hypothesis-extra/hypothesis-fakefactory/
-pip install --upgrade hypothesis-extra/hypothesis-pytest/
-pip install --upgrade hypothesis-extra/hypothesis-django/
-pip install --upgrade hypothesis-extra/hypothesis-numpy/
 python -m pytest --capture=no --strict tests/ --durations=20
-python -m pytest --capture=no --strict hypothesis-extra/*/tests/ --durations=20
 
-for f in hypothesis-extra/*/manage.py; do
-    d=$(dirname $f)
-    pushd $d
-        python manage.py test
-    popd
+for extra in "datetime fakefactory pytest django"; do
+    pip install --upgrade hypothesis-extra/hypothesis-$extra/
+    python -m pytest hypothesis-extra/hypothesis-$extra tests --durations=20
 done
+
+pushd hypothesis-extra/hypothesis-django
+    python manage.py test
+popd
+
+if [ "$(python -c 'import platform; print(platform.python_implementation())')" != "PyPy" ]; then
+    pip install --upgrade hypothesis-extra/hypothesis-numpy/
+    python -m pytest hypothesis-extra/hypothesis-numpy tests
+fi
--- hypothesis-extra/hypothesis-numpy/setup.py
+++ hypothesis-extra/hypothesis-numpy/setup.py
@@ -13,7 +13,6 @@
 from distutils.core import setup
 from setuptools import find_packages
 import os
-import platform
 
 
 def local_file(name):
@@ -22,12 +21,6 @@ def local_file(name):
 SOURCE = local_file("src")
 REQUIREMENTS = local_file("requirements.txt")
 README = local_file("README.rst")
-
-install_requires = [
-    "hypothesis>=1.3,<1.3.99",
-]
-if platform.python_implementation() == 'CPython':
-    install_requires.append("numpy>=1.9.0,<1.9.99")
 
 setup(
     name='hypothesis-numpy',
@@ -39,7 +32,10 @@ setup(
     url='https://github.com/DRMacIver/hypothesis',
     license='MPL v2',
     description='Adds support for generating datetime to Hypothesis',
-    install_requires=install_requires,
+    install_requires=[
+        "hypothesis>=1.3,<1.3.99",
+        "numpy>=1.9.0,<1.9.99",
+    ],
     long_description=open(README).read(),
     entry_points={
         'hypothesis.extra': 'hypothesisnumpy = hypothesisnumpy'
