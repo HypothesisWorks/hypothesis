7d93025e5e1600f42c5eda86cea04ff0d5c31657
David R. MacIver <david@drmaciver.com>
2015-04-22 23:37:40 +0200

    Continuing to yo yo on numpy support

--- basic-test.sh
+++ basic-test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e -o xtrace
 
-if [ "$(python -c 'import platform; print(platform.python_implementation())')" == "PyPy" ]; then
-    pypy -m pip install git+https://bitbucket.org/pypy/numpy.git
-fi
-
-for extra in datetime fakefactory pytest django numpy; do
+for extra in datetime fakefactory pytest django ; do
     pip install --upgrade hypothesis-extra/hypothesis-$extra/
     python -m pytest hypothesis-extra/hypothesis-$extra/tests --durations=20
 done
+
+if [ "$(python -c 'import platform; print(platform.python_implementation())')" != "PyPy" ]; then
+    pip install --upgrade hypothesis-extra/hypothesis-numpy/
+    python -m pytest hypothesis-extra/hypothesis-numpy/tests --durations=20
+fi
 
 pushd hypothesis-extra/hypothesis-django
     python manage.py test
