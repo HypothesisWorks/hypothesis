fc3820dfa529f5dbf679cce42b31941cc3b1df20
David R. MacIver <david@drmaciver.com>
2015-04-22 23:39:58 +0200

    Special case django too

--- basic-test.sh
+++ basic-test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e -o xtrace
 
-for extra in datetime fakefactory pytest django ; do
+for extra in datetime fakefactory pytest ; do
     pip install --upgrade hypothesis-extra/hypothesis-$extra/
-    python -m pytest hypothesis-extra/hypothesis-$extra/tests --durations=20
 done
 
 if [ "$(python -c 'import platform; print(platform.python_implementation())')" != "PyPy" ]; then
     pip install --upgrade hypothesis-extra/hypothesis-numpy/
     python -m pytest hypothesis-extra/hypothesis-numpy/tests --durations=20
 fi
+
+pip install --upgrade hypothesis-extra/hypothesis-django/
 
 pushd hypothesis-extra/hypothesis-django
     python manage.py test
