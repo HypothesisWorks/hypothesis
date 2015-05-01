247f633baa1679fef35b0901d5340d65c509da96
David R. MacIver <david@drmaciver.com>
2015-05-01 21:21:48 +0200

    Add a few strategy_test_suite calls for coverage

---
+++ tests/cover/test_strategytests.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+# Copyright (C) 2013-2015 David R. MacIver (david@drmaciver.com)
+
+# This file is part of Hypothesis (https://github.com/DRMacIver/hypothesis)
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+# END HEADER
+
+# most interesting tests of this nature are exected in nocover, but we have
+# a few here to make sure we have coverage of the strategytests module itself.
+
+from __future__ import division, print_function, absolute_import, \
+    unicode_literals
+
+from hypothesis.strategytests import strategy_test_suite
+
+TestBoolSets = strategy_test_suite({bool})
+TestInts = strategy_test_suite(int)
