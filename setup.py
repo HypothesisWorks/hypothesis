be1d9ddb500efe29ed0223dac80158e6fcd348d3
David R. MacIver <david@drmaciver.com>
2015-05-05 09:28:26 +0200

    Bump patch version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class PyTest(TestCommand):
 
 setup(
     name='hypothesis',
-    version='1.4.0',
+    version='1.4.1',
     author='David R. MacIver',
     author_email='david@drmaciver.com',
     packages=find_packages(SOURCE),
