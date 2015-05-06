8ddcf01dae534ec82ed1bad1b683d9b50b20ee9b
David R. MacIver <david@drmaciver.com>
2015-05-06 09:36:09 +0200

    Don't warn on BasicStrategy.

--- tests/cover/test_deprecation.py
+++ tests/cover/test_deprecation.py
@@ -17,6 +17,7 @@ import pytest
 from hypothesis import given, strategy
 from hypothesis.specifiers import just
 from hypothesis.strategies import booleans
+from hypothesis.searchstrategy.basic import BasicStrategy
 
 
 def test_strategy_does_not_warn_on_strategies(recwarn):
@@ -38,3 +39,21 @@ def test_given_does_not_warn_when_using_strategies_directly(recwarn):
     foo()
     with pytest.raises(AssertionError):
         recwarn.pop(DeprecationWarning)
+
+
+class LocalBasicStrategy(BasicStrategy):
+
+    def generate(self, random, pv):
+        return 'hi'
+
+
+def test_does_not_warn_for_basic_strategy_classes(recwarn):
+    strategy(LocalBasicStrategy)
+    with pytest.raises(AssertionError):
+        print(recwarn.pop(DeprecationWarning))
+
+
+def test_does_not_warn_for_basic_strategy_instances(recwarn):
+    strategy(LocalBasicStrategy())
+    with pytest.raises(AssertionError):
+        print(recwarn.pop(DeprecationWarning))
