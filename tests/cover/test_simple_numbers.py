91be919de6f037bd4b37154370debe8fb9267584
David R. MacIver <david@drmaciver.com>
2015-05-06 12:57:54 +0200

    test_simple_numbers now runs in strict

--- tests/cover/test_simple_numbers.py
+++ tests/cover/test_simple_numbers.py
@@ -18,20 +18,19 @@ import math
 
 import pytest
 from hypothesis import find
-from hypothesis.specifiers import integers_from, floats_in_range, \
-    integers_in_range
+from hypothesis.strategies import lists, floats, integers, complexes
 from hypothesis.searchstrategy.numbers import is_integral
-from hypothesis.searchstrategy.strategies import BadData, strategy
+from hypothesis.searchstrategy.strategies import BadData
 
 
 def test_minimize_negative_int():
-    assert find(int, lambda x: x < 0) == -1
-    assert find(int, lambda x: x < -1) == -2
+    assert find(integers(), lambda x: x < 0) == -1
+    assert find(integers(), lambda x: x < -1) == -2
 
 
 def test_positive_negative_int():
-    assert find(int, lambda x: x > 0) == 1
-    assert find(int, lambda x: x > 1) == 2
+    assert find(integers(), lambda x: x > 0) == 1
+    assert find(integers(), lambda x: x > 1) == 2
 
 
 boundaries = pytest.mark.parametrize('boundary', [0, 1, 11, 23, 64, 10000])
@@ -39,41 +38,41 @@ boundaries = pytest.mark.parametrize('boundary', [0, 1, 11, 23, 64, 10000])
 
 @boundaries
 def test_minimizes_int_down_to_boundary(boundary):
-    assert find(int, lambda x: x >= boundary) == boundary
+    assert find(integers(), lambda x: x >= boundary) == boundary
 
 
 @boundaries
 def test_minimizes_int_up_to_boundary(boundary):
-    assert find(int, lambda x: x <= -boundary) == -boundary
+    assert find(integers(), lambda x: x <= -boundary) == -boundary
 
 
 @boundaries
 def test_minimizes_ints_from_down_to_boundary(boundary):
     assert find(
-        integers_from(boundary - 10), lambda x: x >= boundary) == boundary
+        integers(min_value=boundary - 10), lambda x: x >= boundary) == boundary
 
-    assert find(integers_from(boundary), lambda x: True) == boundary
+    assert find(integers(min_value=boundary), lambda x: True) == boundary
 
 
 @boundaries
 def test_minimizes_integer_range_to_boundary(boundary):
     assert find(
-        integers_in_range(boundary, boundary + 100), lambda x: True
+        integers(boundary, boundary + 100), lambda x: True
     ) == boundary
 
 
 def test_single_integer_range_is_range():
-    assert find(integers_in_range(1, 1), lambda x: True) == 1
+    assert find(integers(1, 1), lambda x: True) == 1
 
 
 def test_find_small_number_in_large_range():
     assert find(
-        integers_in_range((-2 ** 32), 2 ** 32), lambda x: x >= 101) == 101
+        integers((-2 ** 32), 2 ** 32), lambda x: x >= 101) == 101
 
 
 def test_find_small_sum_float_list():
     xs = find(
-        [float],
+        lists(floats()),
         lambda x: len(x) >= 10 and sum(x) >= 1.0
     )
     assert sum(xs) <= 2.0
@@ -83,50 +82,50 @@ def test_finds_boundary_floats():
     def f(x):
         print(x)
         return True
-    assert find(floats_in_range(-1, 1), f) == -1
+    assert find(floats(min_value=-1, max_value=1), f) == -1
 
 
 def test_find_non_boundary_float():
-    x = find(floats_in_range(1, 9), lambda x: x > 2)
+    x = find(floats(min_value=1, max_value=9), lambda x: x > 2)
     assert 2 < x < 3
 
 
 def test_can_find_standard_complex_numbers():
-    find(complex, lambda x: x.imag != 0) == 0j
-    find(complex, lambda x: x.real != 0) == 1
+    find(complexes(), lambda x: x.imag != 0) == 0j
+    find(complexes(), lambda x: x.real != 0) == 1
 
 
 def test_minimial_float_is_zero():
-    assert find(float, lambda x: True) == 0.0
+    assert find(floats(), lambda x: True) == 0.0
 
 
 def test_negative_floats_simplify_to_zero():
-    assert find(float, lambda x: x <= -1.0) == -1.0
+    assert find(floats(), lambda x: x <= -1.0) == -1.0
 
 
 def test_find_infinite_float_is_positive():
-    assert find(float, math.isinf) == float('inf')
+    assert find(floats(), math.isinf) == float('inf')
 
 
 def test_can_find_infinite_negative_float():
-    assert find(float, lambda x: x < -sys.float_info.max)
+    assert find(floats(), lambda x: x < -sys.float_info.max)
 
 
 def test_can_find_float_on_boundary_of_representable():
-    find(float, lambda x: x + 1 == x and not math.isinf(x))
+    find(floats(), lambda x: x + 1 == x and not math.isinf(x))
 
 
 def test_minimize_nan():
-    assert math.isnan(find(float, math.isnan))
+    assert math.isnan(find(floats(), math.isnan))
 
 
 def test_minimize_very_large_float():
     t = sys.float_info.max / 2
-    assert t <= find(float, lambda x: x >= t) < float('inf')
+    assert t <= find(floats(), lambda x: x >= t) < float('inf')
 
 
 def test_can_find_float_far_from_integral():
-    find(float, lambda x: not (
+    find(floats(), lambda x: not (
         math.isnan(x) or
         math.isinf(x) or
         is_integral(x * (2 ** 32))
@@ -134,7 +133,7 @@ def test_can_find_float_far_from_integral():
 
 
 def test_can_find_integrish():
-    find(float, lambda x: (
+    find(floats(), lambda x: (
         is_integral(x * (2 ** 32))
         and not is_integral(x * 16)
     ))
@@ -142,7 +141,7 @@ def test_can_find_integrish():
 
 def test_list_of_fractional_float():
     assert set(find(
-        [float], lambda x: len([t for t in x if t >= 1.5]) >= 10
+        lists(floats()), lambda x: len([t for t in x if t >= 1.5]) >= 10
     )) in (
         {1.5},
         {1.5, 2.0},
@@ -151,32 +150,30 @@ def test_list_of_fractional_float():
 
 
 def test_minimal_fractional_float():
-    assert find(float, lambda x: x >= 1.5) in (1.5, 2.0)
+    assert find(floats(), lambda x: x >= 1.5) in (1.5, 2.0)
 
 
 def test_minimizes_lists_of_negative_ints_up_to_boundary():
     result = find(
-        [int], lambda x: len([t for t in x if t <= -1]) >= 10)
+        lists(integers()), lambda x: len([t for t in x if t <= -1]) >= 10)
     assert result == [-1] * 10
 
 
 def test_out_of_range_integers_are_bad():
     with pytest.raises(BadData):
-        strategy(integers_in_range(0, 1)).from_basic(-1)
+        integers(0, 1).from_basic(-1)
 
     with pytest.raises(BadData):
-        strategy(integers_from(11)).from_basic(9)
+        integers(min_value=11).from_basic(9)
 
 
 def test_out_of_range_floats_are_bad():
     with pytest.raises(BadData):
-        strategy(floats_in_range(11, 12)).from_basic(
-            strategy(floats_in_range(-1, 1)).to_basic(0.0)
-        )
+        floats(11, 12).from_basic(floats(-1, 1).to_basic(0.0))
 
 
 def test_float_simplicity():
-    s = strategy(float).strictly_simpler
+    s = floats().strictly_simpler
 
     def order(x, y):
         x = float(x)
