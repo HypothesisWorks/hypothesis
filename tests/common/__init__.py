3fa3f6407a7a91af08d6e41527d3981c24855a05
David R. MacIver <david@drmaciver.com>
2015-05-06 13:48:48 +0200

    Force standard types to be a bit smaller

--- tests/common/__init__.py
+++ tests/common/__init__.py
@@ -15,7 +15,7 @@ from collections import namedtuple
 
 import pytest
 
-import hypothesis.settings as hs
+from hypothesis.settings import Settings
 from hypothesis.internal.debug import timeout
 from hypothesis.strategytests import templates_for
 from tests.common.basic import Bitfields
@@ -28,7 +28,7 @@ from hypothesis.searchstrategy.narytree import n_ary_tree
 from hypothesis.utils.show import show
 
 
-settings = hs.Settings(max_examples=100, timeout=4)
+settings = Settings(max_examples=100, timeout=4)
 
 __all__ = ['small_verifier', 'timeout', 'standard_types', 'OrderedPair']
 
@@ -55,43 +55,43 @@ ABC = namedtuple('ABC', ('a', 'b', 'c'))
 def abc(x, y, z):
     return tuples(x, y, z, tuple_class=ABC)
 
-
-standard_types = [
-    basic(Bitfields),
-    EvalledIntStream,
-    lists(max_size=0), tuples(), sets(max_size=0), frozensets(max_size=0),
-    dictionaries(),
-    n_ary_tree(booleans(), booleans(), booleans()),
-    abc(booleans(), booleans(), booleans()),
-    abc(booleans(), booleans(), integers()),
-    templates_for(one_of(*map(just, hrange(10)))),
-    dictionaries({'a': integers(), 'b': booleans()}),
-    one_of(integers(), tuples(booleans())),
-    sampled_from(range(10)),
-    one_of(just('a'), just('b'), just('c')),
-    sampled_from(('a', 'b', 'c')),
-    integers(),
-    integers(min_value=3),
-    integers(min_value=(-2 ** 32), max_value=(2 ** 64)),
-    floats(), floats(min_value=-2.0, max_value=3.0),
-    floats(min_value=3.14, max_value=3.14),
-    text(), binary(),
-    booleans(),
-    tuples(booleans(), booleans()),
-    frozensets(integers()),
-    complexes(),
-    fractions(),
-    decimals(),
-    lists(lists(booleans())),
-    ordered_pair, constant_list(integers()),
-    streaming(integers()).map(lambda x: list(x[:2]) and x),
-    integers().filter(lambda x: abs(x) > 100),
-    floats(min_value=-sys.float_info.max, max_value=sys.float_info.max),
-    none(), randoms(),
-    tuples().flatmap(lambda x: EvalledIntStream),
-    templates_for(integers(min_value=0, max_value=0).flatmap(
-        lambda x: integers(min_value=0, max_value=0))),
-]
+with Settings(average_list_length=10.0):
+    standard_types = [
+        basic(Bitfields),
+        EvalledIntStream,
+        lists(max_size=0), tuples(), sets(max_size=0), frozensets(max_size=0),
+        dictionaries(),
+        n_ary_tree(booleans(), booleans(), booleans()),
+        abc(booleans(), booleans(), booleans()),
+        abc(booleans(), booleans(), integers()),
+        templates_for(one_of(*map(just, hrange(10)))),
+        dictionaries({'a': integers(), 'b': booleans()}),
+        one_of(integers(), tuples(booleans())),
+        sampled_from(range(10)),
+        one_of(just('a'), just('b'), just('c')),
+        sampled_from(('a', 'b', 'c')),
+        integers(),
+        integers(min_value=3),
+        integers(min_value=(-2 ** 32), max_value=(2 ** 64)),
+        floats(), floats(min_value=-2.0, max_value=3.0),
+        floats(min_value=3.14, max_value=3.14),
+        text(), binary(),
+        booleans(),
+        tuples(booleans(), booleans()),
+        frozensets(integers()),
+        complexes(),
+        fractions(),
+        decimals(),
+        lists(lists(booleans())),
+        ordered_pair, constant_list(integers()),
+        streaming(integers()).map(lambda x: list(x[:2]) and x),
+        integers().filter(lambda x: abs(x) > 100),
+        floats(min_value=-sys.float_info.max, max_value=sys.float_info.max),
+        none(), randoms(),
+        tuples().flatmap(lambda x: EvalledIntStream),
+        templates_for(integers(min_value=0, max_value=0).flatmap(
+            lambda x: integers(min_value=0, max_value=0))),
+    ]
 
 
 def parametrize(args, values):
