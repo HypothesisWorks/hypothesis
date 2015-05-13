110e1b8a73583fd6024a29d16169476b3358828a
David R. MacIver <david@drmaciver.com>
2015-05-13 10:08:49 +0200

    Explicitly set LC_ALL in linting

--- lint.sh
+++ lint.sh
@@ -2,6 +2,8 @@
 
 set -o xtrace -e
 
+export LC_ALL=en_GB.UTF-8
+
 python enforce_header.py
 isort -p hypothesis -ls -m 2 -w 75 \
     -a  "from __future__ import absolute_import, print_function, unicode_literals, division" \
