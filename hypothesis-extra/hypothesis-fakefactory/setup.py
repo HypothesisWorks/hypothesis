851933fd236571c6ef442b303bde3e3b60ab8c2a
David R. MacIver <david@drmaciver.com>
2015-06-08 16:27:45 +0100

    Version bumps for ff and django in prep for release

--- hypothesis-extra/hypothesis-fakefactory/setup.py
+++ hypothesis-extra/hypothesis-fakefactory/setup.py
@@ -38,7 +38,7 @@ README = local_file("README.rst")
 
 setup(
     name='hypothesis-fakefactory',
-    version='0.10.0',
+    version='0.11.0',
     author='David R. MacIver',
     author_email='david@drmaciver.com',
     packages=find_packages(SOURCE),
