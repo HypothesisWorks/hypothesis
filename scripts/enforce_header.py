639f1e138c29369803870ec28123259dabd9a1b8
David R. MacIver <david@drmaciver.com>
2015-06-10 10:48:56 +0100

    Add custom git merge tool

--- scripts/enforce_header.py
+++ scripts/enforce_header.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import sys
 
 HEADER_FILE = "scripts/header.py"
 
@@ -25,20 +26,36 @@ def main():
     rootdir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     print("cd %r" % (rootdir,))
     os.chdir(rootdir)
-    files = all_python_files()
-    files.remove("scripts/enforce_header.py")
+    if len(sys.argv) > 1:
+        files = sys.argv[1:]
+    else:
+        files = all_python_files()
+    try:
+        files.remove("scripts/enforce_header.py")
+    except ValueError:
+        pass
 
     for f in files:
         print(f)
         lines = []
         with open(f, encoding="utf-8") as o:
+            shebang = None
+            first = True
             for l in o.readlines():
+                if first:
+                    first = False
+                    if l[:2] == '#!':
+                        shebang = l
+                        continue
                 if 'END HEADER' in l:
                     lines = []
                 else:
                     lines.append(l)
         source = ''.join(lines).strip()
         with open(f, "w", encoding="utf-8") as o:
+            if shebang is not None:
+                o.write(shebang)
+                o.write("\n")
             o.write(HEADER_SOURCE)
             o.write("\n\n")
             o.write(source)
