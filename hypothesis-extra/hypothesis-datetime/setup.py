fcacd5f2eb2af7e21ce54fba5042b85879478946
David R. MacIver <david@drmaciver.com>
2015-06-16 18:15:53 +0100

    hypothesis-datetime patch release

--- hypothesis-extra/hypothesis-datetime/setup.py
+++ hypothesis-extra/hypothesis-datetime/setup.py
@@ -42,7 +42,7 @@ README = local_file("README.rst")
 
 setup(
     name='hypothesis-datetime',
-    version='1.6.0',
+    version='1.6.1',
     author='David R. MacIver',
     author_email='david@drmaciver.com',
     packages=find_packages(SOURCE),
