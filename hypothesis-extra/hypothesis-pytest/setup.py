30220c87959d2a9bffc0a14fb3b4fd5aaa751514
David R. MacIver <david@drmaciver.com>
2015-06-28 11:05:58 +0100

    Add 2.6 classifiers to appropriate setup.pys

--- hypothesis-extra/hypothesis-pytest/setup.py
+++ hypothesis-extra/hypothesis-pytest/setup.py
@@ -68,6 +68,7 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
