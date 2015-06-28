ef8b84219d3d33b131ed8f3b2069cdbcbdebebba
David R. MacIver <david@drmaciver.com>
2015-06-28 16:42:37 +0100

    Remove now unneeded function

--- src/hypothesis/strategies.py
+++ src/hypothesis/strategies.py
@@ -120,18 +120,6 @@ def is_negative(x):
     return math.copysign(1, x) < 0
 
 
-def count_between_floats(x, y):
-    assert x <= y
-    if is_negative(x):
-        if is_negative(y):
-            return float_to_int(x) - float_to_int(y) + 1
-        else:
-            return count_between_floats(x, -0.0) + count_between_floats(0.0, y)
-    else:
-        assert not is_negative(y)
-        return float_to_int(y) - float_to_int(x) + 1
-
-
 def float_to_int(value):
     return (
         struct.unpack(b'!Q', struct.pack(b'!d', value))[0]
