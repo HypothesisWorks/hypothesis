97a2c402a44b836c52b59f2abb424ae16c2e44c7
David R. MacIver <david@drmaciver.com>
2015-07-10 09:12:32 +0100

    Add bare Python :: 3 to list

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
