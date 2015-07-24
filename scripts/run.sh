5d818b6e277bb2589152ab5b302aafa0b43fee59
David R. MacIver <david@drmaciver.com>
2015-07-23 23:06:04 -0400

    Have another attempt at using setrlimit to control memory

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,12 +3,6 @@
 set -e
 set -x
 
-if [ "$TOXENV" != "pypy" ]; then
-    if [ "$TOXENV" != "pypy3" ];  then
-        ulimit -m 2000000 -v 2000000
-    fi
-fi
-
 export PATH="$HOME/snakepit:$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv local 3.4.3
--- tests/conftest.py
+++ tests/conftest.py
@@ -34,8 +34,8 @@ Settings.default.strict = True
 
 try:
     import resource
-    MAX_MEMORY = 10
-    resource.setrlimit(resource.RLIMIT_DATA, (MAX_MEMORY, MAX_MEMORY))
+    MAX_MEMORY = 10 ** 9
+    resource.setrlimit(resource.RLIMIT_AS, (MAX_MEMORY, MAX_MEMORY))
 except ImportError:
     pass
 
