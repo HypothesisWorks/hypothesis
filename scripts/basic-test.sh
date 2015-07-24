d7573ae351e9650a56dd97ade497e9fff2600adf
David R. MacIver <david@drmaciver.com>
2015-07-24 08:12:16 -0400

    Run a restricted set of tests but for all platforms on OSX

--- scripts/basic-test.sh
+++ scripts/basic-test.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 set -e -o xtrace
 
+# We run a reduced set of tests on OSX mostly so the CI runs in vaguely
+# reasonable time.
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    DARWIN=true
+else
+    DARWIN=false
+fi
+
 python -c '
 import os
 for k, v in sorted(dict(os.environ).items()):
     print("%s=%s" % (k, v))
 '
+
 python -m pytest tests/cover
-for f in tests/nocover/*.py; do
-  python -m pytest $f
-done
+
+if [ "$DARWIN" != true ]; then
+  for f in tests/nocover/*.py; do
+    python -m pytest $f
+  done
+fi
 
 pip install .[datetime]
 python -m pytest tests/datetime/
 pip uninstall -y pytz
 
+if [ "$DARWIN" = true ] then
+  exit 0
+fi
 
 # fake-factory doesn't have a correct universal wheel
 pip install --no-use-wheel .[fakefactory]
