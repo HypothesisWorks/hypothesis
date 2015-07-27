bb8dac44ae092d178b960be25b3530ff311285e8
David R. MacIver <david@drmaciver.com>
2015-07-27 12:08:24 -0400

    Force the size down to more consistently trigger exceeding limit

--- tests/cover/test_recursive.py
+++ tests/cover/test_recursive.py
@@ -146,7 +146,8 @@ def test_can_simplify_hard_recursive_data_into_boolean_alternative(rnd):
 def test_can_flatmap_to_recursive_data():
     stuff = st.lists(st.integers(), min_size=1).flatmap(
         lambda elts: st.recursive(
-            st.sampled_from(elts), lambda x: st.lists(x, average_size=25)
+            st.sampled_from(elts), lambda x: st.lists(x, average_size=25),
+            max_leaves=25
         ))
 
     def flatten(x):
