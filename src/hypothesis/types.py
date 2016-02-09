b7fbb4eaf36bb01ae539d49376340ff393aefe38
David R. MacIver <david@drmaciver.com>
2016-02-09 10:15:06 +0000

    Remove Stream type

--- src/hypothesis/types.py
+++ src/hypothesis/types.py
@@ -16,11 +16,7 @@
 
 from __future__ import division, print_function, absolute_import
 
-import inspect
 from random import Random
-from itertools import islice
-
-from hypothesis.errors import InvalidArgument
 
 
 class RandomWithSeed(Random):
@@ -47,81 +43,3 @@ class RandomWithSeed(Random):
 
     def __repr__(self):
         return u'RandomWithSeed(%s)' % (self.seed,)
-
-
-class Stream(object):
-
-    """A stream is a possibly infinite list. You can index into it, and you can
-    iterate over it, but you can't ask its length and iterating over it will
-    not necessarily terminate.
-
-    Behind the scenes streams are backed by a generator, but they "remember"
-    the values as they evaluate them so you can replay them later.
-
-    Internally Hypothesis uses the fact that you can tell how much of a stream
-    has been evaluated, but you shouldn't use that. The only public APIs of
-    a Stream are that you can index, slice, and iterate it.
-
-    """
-
-    def __init__(self, generator=None):
-        if generator is None:
-            generator = iter(())
-        elif not inspect.isgenerator(generator):
-            generator = iter(generator)
-        self.generator = generator
-        self.fetched = []
-
-    def map(self, f):
-        return Stream(f(v) for v in self)
-
-    def __iter__(self):
-        i = 0
-        while i < len(self.fetched):
-            yield self.fetched[i]
-            i += 1
-        for v in self.generator:
-            self.fetched.append(v)
-            yield v
-
-    def __getitem__(self, key):
-        if isinstance(key, slice):
-            return Stream(islice(
-                iter(self),
-                key.start, key.stop, key.step
-            ))
-
-        if not isinstance(key, int):
-            raise InvalidArgument(u'Cannot index stream with %s' % (
-                type(key).__name__,))
-        self._thunk_to(key + 1)
-        return self.fetched[key]
-
-    def _thunk_to(self, i):
-        it = iter(self)
-        try:
-            while len(self.fetched) < i:
-                next(it)
-        except StopIteration:
-            raise IndexError(
-                u'Index %d out of bounds for finite stream of length %d' % (
-                    i, len(self.fetched)
-                )
-            )
-
-    def _thunked(self):
-        return len(self.fetched)
-
-    def __repr__(self):
-        if not self.fetched:
-            return u'Stream(...)'
-
-        return u'Stream(%s, ...)' % (
-            u', '.join(map(repr, self.fetched))
-        )
-
-    def __deepcopy__(self, table):
-        return self
-
-    def __copy__(self):
-        return self
