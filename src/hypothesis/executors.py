8de9e8dc749d35225b5a9c330c5280df21362119
David R. MacIver <david@drmaciver.com>
2016-02-15 13:47:09 +0000

    Simplify executors logic.

--- src/hypothesis/executors.py
+++ src/hypothesis/executors.py
@@ -17,44 +17,24 @@
 from __future__ import division, print_function, absolute_import
 
 
-def default_executor(function):  # pragma: nocover
-    raise NotImplementedError()  # We don't actually use this any more
-
-
-def setup_teardown_executor(setup, teardown):
-    setup = setup or (lambda: None)
-    teardown = teardown or (lambda ex: None)
-
-    def execute(function):
-        token = None
-        try:
-            token = setup()
-            return function()
-        finally:
-            teardown(token)
-    return execute
-
-
-def executor(runner):
-    try:
-        return runner.execute_example
-    except AttributeError:
-        pass
-
-    if (
-        hasattr(runner, 'setup_example') or
-        hasattr(runner, 'teardown_example')
-    ):
-        return setup_teardown_executor(
-            getattr(runner, 'setup_example', None),
-            getattr(runner, 'teardown_example', None),
-        )
-
-    return default_executor
-
-
 def default_new_style_executor(data, function):
     return function(data)
+
+
+class setup_teardown_executor(object):
+    def __init__(self, setup, teardown):
+        self.setup = setup
+        self.teardown = teardown
+
+    def __call__(self, data, function):
+        try:
+            if self.setup is not None:
+                self.setup()
+                return function(data)
+        finally:
+            if self.teardown is not None:
+                self.teardown()
+
 
 
 class TestRunner(object):
@@ -69,10 +49,12 @@ def new_style_executor(runner):
     if isinstance(runner, TestRunner):
         return runner.hypothesis_execute_example_with_data
 
-    old_school = executor(runner)
-    if old_school is default_executor:
-        return default_new_style_executor
-    else:
-        return lambda data, function: old_school(
-            lambda: function(data)
+    if (
+        hasattr(runner, 'setup_example') or
+        hasattr(runner, 'teardown_example')
+    ):
+        return setup_teardown_executor(
+            getattr(runner, 'setup_example', None),
+            getattr(runner, 'teardown_example', None),
         )
+    return default_new_style_executor
