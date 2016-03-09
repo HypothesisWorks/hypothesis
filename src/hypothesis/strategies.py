HypothesisWorks/hypothesis

commit 2d195a0d54aceabb348d55ca611bdf92ddabd7ae

Revert "Rip out choices and streaming"
This reverts commit 9a4271446986dc8fef5a3fab94d7f8af77a0eafd.

Conflicts:
	src/hypothesis/strategies.py
	tests/cover/test_stateful.py
	tests/nocover/test_descriptortests.py
	tests/nocover/test_strategy_state.py

src/hypothesis/strategies.py
@@ -32,6 +32,7 @@ from hypothesis.searchstrategy.reprwrapper import ReprWrapperStrategy
 __all__ = [
     'just', 'one_of',
     'none',
+    'choices', 'streaming',
     'booleans', 'integers', 'floats', 'complex_numbers', 'fractions',
     'decimals',
     'characters', 'text', 'binary',
@@ -42,6 +43,7 @@ __all__ = [
     'randoms', 'random_module',
     'recursive', 'composite',
     'shared',
+    'recursive', 'composite',
 ]
 
 _strategies = set()
@@ -514,6 +516,21 @@ def dictionaries(
 
 @cacheable
 @defines_strategy
+def streaming(elements):
+    """Generates an infinite stream of values where each value is drawn from
+    elements.
+
+    The result is iterable (the iterator will never terminate) and
+    indexable.
+
+    """
+    check_strategy(elements)
+    from hypothesis.searchstrategy.streams import StreamStrategy
+    return StreamStrategy(elements)
+
+
+@cacheable
+@defines_strategy
 def characters(whitelist_categories=None, blacklist_categories=None,
                blacklist_characters=None, min_codepoint=None,
                max_codepoint=None):
@@ -796,6 +813,47 @@ def shared(base, key=None):
     """
     from hypothesis.searchstrategy.shared import SharedStrategy
     return SharedStrategy(base, key)
+
+
+@cacheable
+def choices():
+    """Strategy that generates a function that behaves like random.choice.
+
+    Will note choices made for reproducibility.
+
+    """
+    from hypothesis.control import note, current_build_context
+    from hypothesis.internal.conjecture.utils import choice
+
+    class Chooser(object):
+
+        def __init__(self, build_context, data):
+            self.build_context = build_context
+            self.data = data
+            self.choice_count = 0
+
+        def __call__(self, values):
+            if not values:
+                raise IndexError('Cannot choose from empty sequence')
+            result = choice(self.data, values)
+            with self.build_context.local():
+                self.choice_count += 1
+                note('Choice #%d: %r' % (self.choice_count, result))
+            return result
+
+        def __repr__(self):
+            return 'choice'
+
+    class ChoiceStrategy(SearchStrategy):
+
+        def do_draw(self, data):
+            return Chooser(current_build_context(), data)
+
+    return ReprWrapperStrategy(
+        shared(
+            ChoiceStrategy(),
+            key='hypothesis.strategies.chooser.choice_function'
+        ), 'chooser()')
 
 
 @cacheable

src/hypothesis/core.py
@@ -593,7 +593,8 @@ def find(specifier, condition, settings=None, random=None, database_key=None):
     run_time = time.time() - start
     if runner.last_data.status == Status.INTERESTING:
         with BuildContext():
-            return TestData.for_buffer(runner.last_data.buffer).draw(search)
+            return TestData.for_buffer(
+                runner.last_data.buffer, expand=True).draw(search)
     if runner.valid_examples <= settings.min_satisfying_examples:
         if settings.timeout > 0 and run_time > settings.timeout:
             raise Timeout((

src/hypothesis/internal/conjecture/data.py
@@ -46,12 +46,27 @@ global_test_counter = 0
 class TestData(object):
 
     @classmethod
-    def for_buffer(self, buffer):
-        return TestData(
-            max_length=len(buffer),
-            draw_bytes=lambda data, n, distribution:
-            buffer[data.index:data.index + n]
-        )
+    def for_buffer(self, buffer, expand=False):
+        if expand:
+            from random import Random
+            rnd = Random(buffer)
+
+            def db(data, n, distribution):
+                if data.index + n <= len(buffer):
+                    return buffer[data.index:data.index + n]
+                else:
+                    return distribution(rnd, n)
+
+            return TestData(
+                max_length=2 ** 64,
+                draw_bytes=db
+            )
+        else:
+            return TestData(
+                max_length=len(buffer),
+                draw_bytes=lambda data, n, distribution:
+                buffer[data.index:data.index + n]
+            )
 
     def __init__(self, max_length, draw_bytes):
         self.max_length = max_length

tests/common/__init__.py
@@ -25,7 +25,7 @@ except ImportError:
 from hypothesis._settings import settings
 from hypothesis.internal.debug import timeout
 from hypothesis.strategies import integers, floats, just, one_of, \
-    sampled_from, lists, booleans, dictionaries, tuples, \
+    sampled_from, streaming, lists, booleans, dictionaries, tuples, \
     frozensets, complex_numbers, sets, text, binary, decimals, fractions, \
     none, randoms, builds, fixed_dictionaries, recursive
 
@@ -47,6 +47,8 @@ def constant_list(strat):
     )
 
 
+EvalledIntStream = streaming(integers()).map(lambda x: list(x[:3]) and x)
+
 ABC = namedtuple('ABC', ('a', 'b', 'c'))
 
 
@@ -55,6 +57,7 @@ def abc(x, y, z):
 
 with settings(strict=False):
     standard_types = [
+        EvalledIntStream,
         lists(max_size=0), tuples(), sets(max_size=0), frozensets(max_size=0),
         fixed_dictionaries({}),
         abc(booleans(), booleans(), booleans()),
@@ -86,9 +89,11 @@ with settings(strict=False):
         lists(lists(booleans(), average_size=100)),
         lists(floats(0.0, 0.0), average_size=1.0),
         ordered_pair, constant_list(integers()),
+        streaming(integers()).map(lambda x: list(x[:2]) and x),
         integers().filter(lambda x: abs(x) > 100),
         floats(min_value=-sys.float_info.max, max_value=sys.float_info.max),
         none(), randoms(),
+        tuples().flatmap(lambda x: EvalledIntStream),
         booleans().flatmap(lambda x: booleans() if x else complex_numbers()),
         recursive(
             base=booleans(), extend=lambda x: lists(x, max_size=3),

tests/cover/test_choices.py
@@ -1,0 +1,48 @@
+# coding=utf-8
+#
+# This file is part of Hypothesis (https://github.com/DRMacIver/hypothesis)
+#
+# Most of this work is copyright (C) 2013-2015 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# https://github.com/DRMacIver/hypothesis/blob/master/CONTRIBUTING.rst for a
+# full list of people who may hold copyright, and consult the git log if you
+# need to determine who owns an individual contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+from __future__ import division, print_function, absolute_import
+
+import hypothesis.strategies as st
+from hypothesis import find, given
+from tests.common.utils import raises
+
+
+def test_exhaustion():
+    @given(st.lists(st.text(), min_size=10), st.choices())
+    def test(ls, choice):
+        while ls:
+            l = choice(ls)
+            assert l in ls
+            ls.remove(l)
+    test()
+
+
+@given(st.choices(), st.choices())
+def test_choice_is_shared(choice1, choice2):
+    assert choice1 is choice2
+
+
+def test_can_use_a_choice_function_after_find():
+    c = find(st.choices(), lambda c: True)
+    ls = [1, 2, 3]
+    assert c(ls) in ls
+
+
+def test_choice_raises_index_error_on_empty():
+    c = find(st.choices(), lambda c: True)
+    with raises(IndexError):
+        c([])

tests/cover/test_conjecture_test_data.py
@@ -66,6 +66,16 @@ def test_draw_past_end_sets_overflow():
     assert x.status == Status.OVERRUN
 
 
+def test_can_draw_indefinitely_if_expand():
+    x = TestData.for_buffer(b'', expand=True)
+    x.draw_bytes(100)
+
+
+def test_draws_initial_data_if_expand():
+    x = TestData.for_buffer(b'foo', expand=True)
+    assert x.draw_bytes(3) == b'foo'
+
+
 def test_notes_repr():
     x = TestData.for_buffer(b'')
     x.note(b'hi')

tests/cover/test_direct_strategies.py
@@ -140,6 +140,11 @@ def test_tuples_raise_error_on_bad_kwargs():
         ds.tuples(stuff=u'things')
 
 
+def test_streaming_streams():
+    for v in ds.streaming(ds.integers(max_value=1000)).example()[:10]:
+        assert v <= 1000
+
+
 @given(ds.lists(ds.booleans(), min_size=10, max_size=10))
 def test_has_specified_length(xs):
     assert len(xs) == 10

tests/cover/test_find.py
@@ -25,7 +25,7 @@ from hypothesis import settings as Settings
 from hypothesis import find
 from hypothesis.errors import Timeout, NoSuchExample
 from hypothesis.strategies import lists, floats, booleans, integers, \
-    dictionaries
+    streaming, dictionaries
 
 
 def test_can_find_an_int():
@@ -48,6 +48,12 @@ def test_can_find_nans():
         assert math.isnan(x[0])
     else:
         assert 2 <= len(x) <= 3
+
+
+def test_find_streaming_int():
+    n = 50
+    r = find(streaming(integers()), lambda x: any(t >= 1 for t in list(x[:n])))
+    assert sorted(r[:n]) == [0] * (n - 1) + [1]
 
 
 def test_raises_when_no_example():

commit a02997d561f641ca5d16177096b3b27674de4821

Remove duplicated __all__ exports
These seem to have been introduced accidentally in 2d195a0,
and seem to cause Sphinx to duplicate their automodule documentation,
as can be seen here:

http://hypothesis.readthedocs.org/en/3.1.0/data.html#hypothesis.strategies.recursive
http://hypothesis.readthedocs.org/en/3.1.0/data.html#hypothesis.strategies.composite

src/hypothesis/strategies.py
@@ -45,7 +45,6 @@ __all__ = [
     'randoms', 'random_module',
     'recursive', 'composite',
     'shared', 'runner',
-    'recursive', 'composite',
 ]
 
 _strategies = set()