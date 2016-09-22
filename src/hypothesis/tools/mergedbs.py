ce8705a9bab9de28fda3955f112b80349242ccae
David R. MacIver <david@drmaciver.com>
2016-09-22 17:40:16 +0100

    Update copyright range

--- src/hypothesis/tools/mergedbs.py
+++ src/hypothesis/tools/mergedbs.py
@@ -5,7 +5,7 @@
 # This file is part of Hypothesis, which may be found at
 # https://github.com/HypothesisWorks/hypothesis-python
 #
-# Most of this work is copyright (C) 2013-2015 David R. MacIver
+# Most of this work is copyright (C) 2013-2016 David R. MacIver
 # (david@drmaciver.com), but it contains contributions by others. See
 # CONTRIBUTING.rst for a full list of people who may hold copyright, and
 # consult the git log if you need to determine who owns an individual
