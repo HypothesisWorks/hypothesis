1b5817b3d07993767ef84f1a46bbcb757e5d8209
David R. MacIver <david@drmaciver.com>
2016-09-30 17:30:22 +0100

    Add some more compatibility tests

---
+++ tests/pytest/test_compatibility.py
@@ -0,0 +1,51 @@
+# coding=utf-8
+#
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis-python
+#
+# Most of this work is copyright (C) 2013-2016 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# CONTRIBUTING.rst for a full list of people who may hold copyright, and
+# consult the git log if you need to determine who owns an individual
+# contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+from __future__ import division, print_function, absolute_import
+
+from hypothesis import given, assume, strategies as st, settings
+
+
+pytest_plugins = 'pytester'
+
+seen = []
+
+
+@given(st.binary(min_size=10))
+def test_mostly_assumes_false(xs):
+    if not seen:
+        seen.append(xs)
+    assume(xs in seen)
+
+
+def test_basic_failures(testdir):
+    script = testdir.makepyfile("""
+    from hypothesis import given, settings
+    from hypothesis.strategies import integers
+
+    @given(integers())
+    def test_yes(i):
+        assert True
+
+    @settings(database=None)
+    @given(integers())
+    def test_no(i):
+        assert i < 1001
+    """)
+    result = testdir.runpytest(script)
+    assert result.ret != 0
+    assert 'i=1001' in '\n'.join(result.stdout.lines)
