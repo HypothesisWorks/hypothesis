wenzhang61/hypothesis-python

Add `allow_nan` and `allow_infinity` flags to `decimals()`
Resolves #186.

src/hypothesis/strategies.py
@@ -803,21 +803,42 @@ def fractions(min_value=None, max_value=None, max_denominator=None):
 
 @cacheable
 @defines_strategy
-def decimals(min_value=None, max_value=None):
+def decimals(
+    min_value=None, max_value=None, allow_nan=True, allow_infinity=True
+):
     """Generates instances of decimals.Decimal.
 
-    If min_value is not None then all generated values are no less than
-    min_value.
+    - If min_value is not None, all values will be >= min_value.
+    - If max_value is not None, all values will be <= max_value.
+    - If min_value or max_value is not None, it is an error to enable
+      allow_nan.
+    - If both min_value and max_value are not None, it is an error to enable
+      allow_infinity.
 
-    If max_value is not None then all generated values are no greater than
-    max_value.
+    Where not explicitly ruled out by the bounds, all of infinity, -infinity
+    and NaN are possible values generated by this strategy.
 
     """
     check_valid_bound(min_value, 'min_value')
     check_valid_bound(max_value, 'max_value')
     check_valid_interval(min_value, max_value, 'min_value', 'max_value')
 
-    floats_strategy = floats(min_value=min_value, max_value=max_value)
+    if allow_nan and (min_value is not None or max_value is not None):
+        raise InvalidArgument(
+            'Cannot have allow_nan=%r, with min_value or max_value' % (
+                allow_nan
+            ))
+
+    if allow_infinity and (min_value is not None and max_value is not None):
+        raise InvalidArgument(
+            'Cannot have allow_infinity=%r, with both min_value and '
+            'max_value' % (
+                allow_infinity
+            ))
+
+    floats_strategy = floats(
+        min_value=min_value, max_value=max_value,
+        allow_nan=allow_nan, allow_infinity=allow_infinity)
     fractions_strategy = fractions(min_value=min_value, max_value=max_value)
 
     return (

HypothesisWorks/hypothesis

src/hypothesis/strategies.py
@@ -803,21 +803,25 @@ def fractions(min_value=None, max_value=None, max_denominator=None):
 
 @cacheable
 @defines_strategy
-def decimals(min_value=None, max_value=None):
+def decimals(
+    min_value=None, max_value=None, allow_nan=None, allow_infinity=None
+):
     """Generates instances of decimals.Decimal.
 
-    If min_value is not None then all generated values are no less than
-    min_value.
+    - If min_value is not None, all values will be >= min_value.
+    - If max_value is not None, all values will be <= max_value.
+    - If min_value or max_value is not None, it is an error to enable
+      allow_nan.
+    - If both min_value and max_value are not None, it is an error to enable
+      allow_infinity.
 
-    If max_value is not None then all generated values are no greater than
-    max_value.
+    Where not explicitly ruled out by the bounds, all of infinity, -infinity
+    and NaN are possible values generated by this strategy.
 
     """
-    check_valid_bound(min_value, 'min_value')
-    check_valid_bound(max_value, 'max_value')
-    check_valid_interval(min_value, max_value, 'min_value', 'max_value')
-
-    floats_strategy = floats(min_value=min_value, max_value=max_value)
+    floats_strategy = floats(
+        min_value=min_value, max_value=max_value,
+        allow_nan=allow_nan, allow_infinity=allow_infinity)
     fractions_strategy = fractions(min_value=min_value, max_value=max_value)
 
     return (