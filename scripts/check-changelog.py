716e2634ea79da983612903b13cce64b2392e867
David R. MacIver <david@drmaciver.com>
2017-04-20 13:21:44 +0100

    Update inline space

--- scripts/check-changelog.py
+++ scripts/check-changelog.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__)) # noqa
+sys.path.append(os.path.dirname(__file__))  # noqa
 
 import hypothesistooling as tools
 from datetime import datetime
