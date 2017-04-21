e0efb9f1c4ffd0ca899ca7ce78fc2a7f111d76ee
David R. MacIver <david@drmaciver.com>
2017-04-21 08:00:59 +0100

    Also run enforce_header.py on itself

--- scripts/enforce_header.py
+++ scripts/enforce_header.py
@@ -1,6 +1,25 @@
-from datetime import datetime
+# coding=utf-8
+#
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis-python
+#
+# Most of this work is copyright (C) 2013-2017 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# CONTRIBUTING.rst for a full list of people who may hold copyright, and
+# consult the git log if you need to determine who owns an individual
+# contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+from __future__ import division, print_function, absolute_import
+
 import os
 import sys
+from datetime import datetime
 
 HEADER_FILE = "scripts/header.py"
 
@@ -13,10 +32,6 @@ def main():
     rootdir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     os.chdir(rootdir)
     files = sys.argv[1:]
-    try:
-        files.remove("scripts/enforce_header.py")
-    except ValueError:
-        pass
 
     for f in files:
         print(f)
@@ -24,14 +39,16 @@ def main():
         with open(f, encoding="utf-8") as o:
             shebang = None
             first = True
+            header_done = False
             for l in o.readlines():
                 if first:
                     first = False
                     if l[:2] == '#!':
                         shebang = l
                         continue
-                if 'END HEADER' in l:
+                if 'END HEADER' in l and not header_done:
                     lines = []
+                    header_done = True
                 else:
                     lines.append(l)
         source = ''.join(lines).strip()
