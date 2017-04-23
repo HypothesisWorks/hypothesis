70b9a65d87f2fd87b2d03eda6a8d29e34ff6d60a
David R. MacIver <david@drmaciver.com>
2017-04-23 07:47:58 +0100

    More conservative check for source changes

--- scripts/hypothesistooling.py
+++ scripts/hypothesistooling.py
@@ -92,8 +92,15 @@ def changelog():
 
 
 def has_source_changes(version):
+    # Check where we branched off from the version. We're only interested
+    # in whether *we* introduced any source changes, so we check diff from
+    # there rather than the diff to the other side.
+    point_of_divergence = subprocess.check_output([
+        'git', 'merge-base', 'HEAD', version
+    ])
+
     return subprocess.call([
-        'git', 'diff', '--exit-code', version, SRC,
+        'git', 'diff', '--exit-code', point_of_divergence, SRC,
     ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) != 0
 
 
