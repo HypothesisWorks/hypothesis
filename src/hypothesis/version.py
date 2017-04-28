151aa8943739b73c0f981ee529bde4c5d8f57673
Zac-HD <zac.hatfield.dodds@gmail.com>
2017-04-28 16:02:49 +1000

    Add changelog, bump version

--- src/hypothesis/version.py
+++ src/hypothesis/version.py
@@ -17,5 +17,5 @@
 
 from __future__ import division, print_function, absolute_import
 
-__version_info__ = (3, 8, 2)
+__version_info__ = (3, 9, 0)
 __version__ = '.'.join(map(str, __version_info__))
