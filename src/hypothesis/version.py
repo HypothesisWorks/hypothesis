2454e4aa8c3e0808dbe0076a2e1c49f0260a55ea
David R. MacIver <david@drmaciver.com>
2017-05-17 15:57:13 +0100

    Update version

--- src/hypothesis/version.py
+++ src/hypothesis/version.py
@@ -17,5 +17,5 @@
 
 from __future__ import division, print_function, absolute_import
 
-__version_info__ = (3, 11, 4)
+__version_info__ = (3, 11, 5)
 __version__ = '.'.join(map(str, __version_info__))
