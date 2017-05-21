08de6b751272a62111f26f8225bcbf794d19043b
Marco Sirabella <msirabel@gmail.com>
2017-05-21 15:01:31 -0700

    - Commented so much

--- src/hypothesis/extra/types.py
+++ src/hypothesis/extra/types.py
@@ -17,27 +17,31 @@ __module__      = "Hypothesis"
 
 class Type2Strat(dict):
     def __init__(self):
-        for strat_name in dir(st):
-            strat = getattr(st, strat_name)
-            del strat_name
-            if inspect.isfunction(strat):
+        for strat_name in dir(st):  # Iterate through all attributes in module
+            strat = getattr(st, strat_name)  # get attribute of name
+            del strat_name  # No need for the strategy name anymore,
+            if inspect.isfunction(strat):  # If attribute is callable function
                 try:
-                    strat = strat()
+                    strat = strat()  # Call wrapping function
                     if isinstance(
-                            strat,
+                            strat,  # If it is a search strategy
                             hypothesis.searchstrategy.strategies.SearchStrategy
                     ):
-                        self.append(strat)
+                        self.append(strat)  # Add strategy to self
                 except (TypeError, hypothesis.errors.NoExamples):
+                    # If not enough args or not able to get examples
                     pass
 
     def __getitem__(self, item):
-        if isinstance(item, typing._Union):
-            both = item.__args__
-            return self[both[0]] | self[both[1]]
-        return super().__getitem__(item)
+        if isinstance(item, typing._Union):  # If is union of multiple types
+            both = item.__args__  # assign each item of args to both
+            return self[both[0]] | self[both[1]]  # Call __getitem__ for each
+        return super().__getitem__(item)  # If not union just return normally
 
     def append(self, strategy):
+        """
+        Add strategy and respective type as key
+        """
         self[type(strategy.example())] = strategy
 
 
@@ -45,6 +49,15 @@ type2strat = Type2Strat()
 
 
 def infer(func):
+    """
+    a decorator for turning a test function that accepts argument into a
+    randomized test
+
+    This is a offshoot of the given function, rather than accept arguments of
+    what strategies the tests should use, tries to infer based on type
+    annotations of wrapped function
+    """
+
     types = func.__annotations__
     kwargs = {}
     for key, value in types.items():
