9eabbbcc199bff00a7834760ea1712d28de5dcb0
Marco Sirabella <msirabel@gmail.com>
2017-05-21 13:49:18 -0700

    - Now catalogs all strats in hypothesis.strategies

--- src/hypothesis/extra/types.py
+++ src/hypothesis/extra/types.py
@@ -6,6 +6,7 @@ Infer types of test function arguments using type annotations
 
 import hypothesis
 import hypothesis.strategies as st
+import inspect
 
 __author__      = "Marco Sirabella"
 __credits__     = ["Marco Sirabella"]  # Authors and bug reporters
@@ -14,10 +15,20 @@ __module__      = "Hypothesis"
 
 
 type2strat = {}
-ez_strategies = (st.integers(), st.floats(), st.text(), st.binary(),
-st.booleans())
-for strat in ez_strategies:
-    type2strat[type(strat.example())] = strat
+for strat_name in dir(st):
+    strat = getattr(st, strat_name)
+    del strat_name
+    if inspect.isfunction(strat):
+        try:
+            strat = strat()
+            if isinstance(
+                    strat,
+                    hypothesis.searchstrategy.strategies.SearchStrategy
+                    ):
+                type2strat[type(strat.example())] = strat
+        except (TypeError, hypothesis.errors.NoExamples):
+            pass
+
 
 def infer(func):
     types = func.__annotations__
