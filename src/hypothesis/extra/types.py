6c6cfada69e0b0116f27ec07bb9f58e6f0a418a3
Marco Sirabella <msirabel@gmail.com>
2017-05-21 13:36:41 -0700

    - First commit

---
+++ src/hypothesis/extra/types.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Infer types of test function arguments using type annotations
+"""
+
+import hypothesis
+import hypothesis.strategies as st
+
+__author__      = "Marco Sirabella"
+__credits__     = ["Marco Sirabella"]  # Authors and bug reporters
+__email__       = "msirabel@gmail.com"
+__module__      = "Hypothesis"
+
+
+type2strat = {
+        int: st.integers(),
+        float: st.floats(),
+        str: st.text(),
+        bytes: st.binary(),
+        bool: st.booleans(),
+}
+
+def infer(func):
+    types = func.__annotations__
+    kwargs = {}
+    for key, value in types.items():
+        kwargs[key] = type2strat[value]
+    func = hypothesis.given(**kwargs)(func)
+    return func
+
+
+# Testing quickly
+
+@infer
+def test(i: int):
+    print(i)
+
+test()
