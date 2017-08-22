fccfa6f85af995b34897236a1aaa43bfbed850cd
David R. MacIver <david@drmaciver.com>
2017-08-22 10:38:42 +0100

    More argument validation.

--- tests/numpy/test_argument_validation.py
+++ tests/numpy/test_argument_validation.py
@@ -18,9 +18,10 @@
 from __future__ import division, print_function, absolute_import
 
 import pytest
-from hypothesis.errors import InvalidArgument
-import hypothesis.extra.numpy as nps
+
 import hypothesis.strategies as st
+import hypothesis.extra.numpy as nps
+from hypothesis.errors import InvalidArgument
 
 
 def e(a, **kwargs):
@@ -32,12 +33,23 @@ def e(a, **kwargs):
         e(nps.array_dtypes, min_size=2, max_size=1),
         e(nps.array_shapes, min_side=2, max_side=1),
         e(nps.array_shapes, min_dims=3, max_dims=2),
+        e(nps.arrays, dtype=float, shape=(0.5,)),
+        e(nps.arrays, dtype=float, shape=10 ** 5),
         e(nps.arrays, dtype=object, shape=1),
         e(nps.arrays, dtype=object, shape=(), elements=st.none()),
         e(nps.byte_string_dtypes, min_len=-1),
         e(nps.byte_string_dtypes, min_len=2, max_len=1),
+        e(nps.datetime64_dtypes, max_period=11),
+        e(nps.datetime64_dtypes, min_period=11),
+        e(nps.datetime64_dtypes, min_period='Y', max_period='M'),
+        e(nps.timedelta64_dtypes, max_period=11),
+        e(nps.timedelta64_dtypes, min_period=11),
+        e(nps.timedelta64_dtypes, min_period='Y', max_period='M'),
         e(nps.unicode_string_dtypes, min_len=-1),
         e(nps.unicode_string_dtypes, min_len=2, max_len=1),
+        e(nps.unsigned_integer_dtypes, endianness=3),
+        e(nps.unsigned_integer_dtypes, sizes=()),
+        e(nps.unsigned_integer_dtypes, sizes=(3,)),
     ]
 )
 def test_raise_invalid_argument(function, kwargs):
