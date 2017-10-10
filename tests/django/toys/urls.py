589e38d7f1f98ae3e3e6cb426c20bbaa1c3eca69
Zac-HD <zac.hatfield.dodds@gmail.com>
2017-10-10 17:52:26 +1100

    Use Django2 idioms in our tests
    To avoid deprecation errors.

--- tests/django/toys/urls.py
+++ tests/django/toys/urls.py
@@ -20,10 +20,12 @@ from __future__ import division, print_function, absolute_import
 from django.contrib import admin
 from django.conf.urls import url, include
 
+patterns, namespace, name = admin.site.urls
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'toys.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include((patterns, name), namespace=namespace))
 ]
