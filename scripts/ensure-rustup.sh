f074fc4ca3c2b132e4fce76acf33e3c439e6e1cf
David R. MacIver <david@drmaciver.com>
2017-11-03 17:19:00 +0000

    Apparently it's -y not --yes

--- scripts/ensure-rustup.sh
+++ scripts/ensure-rustup.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 if ! which rustup > /dev/null ; then 
-  curl https://sh.rustup.rs -sSf | sh -s -- --yes
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
 fi
 
 if ! rustup show | grep nightly > /dev/null ; then
