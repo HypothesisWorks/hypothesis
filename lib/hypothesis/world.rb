24608b561512f26c635428ec531d13782d7d9bc9
David R. MacIver <david@drmaciver.com>
2017-11-03 09:45:21 +0000

    Use attr_accessor instead

--- lib/hypothesis/world.rb
+++ lib/hypothesis/world.rb
@@ -1,18 +1,9 @@
-
 # frozen_string_literal: true
 
 module Hypothesis
   module World
-    def self.current_engine
-      if !defined? @current_engine
-        nil
-      else
-        @current_engine
-      end
-    end
-
-    def self.current_engine=(engine)
-      @current_engine = engine
+    class <<self
+        attr_accessor :current_engine
     end
   end
 end
