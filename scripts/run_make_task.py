873ee649551834ac8cdd0f843a075e5acf152cb4
Alex Chan <alex@alexwlchan.net>
2017-12-10 00:37:15 +0000

    Decide whether to run the task based on certain conditions

--- scripts/run_make_task.py
+++ scripts/run_make_task.py
@@ -21,5 +21,79 @@ import os
 import subprocess
 
 
+def changed_paths_from_master():
+    """
+    Returns a list of files which have changed in this pull request.
+    """
+    files = set()
+    command = ['git', 'diff', '--name-only'] + list(args)
+    diff_output = subprocess.check_output(command).decode('ascii')
+    for line in diff_output.splitlines():
+        filepath = line.strip()
+        if filepath:
+            files.add(filepath)
+    return files
+
+
+def _is_safe_file(path):
+    """
+    Is this a file which has no effect on test results?
+    """
+    if path.endswith(('.rst', '.ipynb')):
+        return True
+
+    if path.startswith(('src/', 'tests/', 'requirements/', 'setup.py')):
+        return False
+    
+    return False
+
+
+def should_run_task(task):
+    """
+    Given a task name, should we run this task in Travis?  Returns True/False.
+    """
+    event_type = os.environ.get('TRAVIS_EVENT_TYPE')
+    if event_type != 'pull_request':
+        print(
+            'We only skip Travis jobs if the job is a pull request, '
+            'but here event_type=%r.' % event_type
+        )
+        return True
+    
+    # These tests are usually fast; we always run them rather than trying
+    # to keep up-to-date rules of exactly which changed files mean they
+    # should run.
+    if task in [
+        'check-pyup-yml',
+        'check-release-file',
+        'check-shellcheck',
+        'documentation',
+        'lint',
+    ]:
+        print('We always run the %s task.' % task)
+        return True
+    
+    # The remaining tasks are all some sort of test of Hypothesis 
+    # functionality.  Since it's better to run tests when we don't need to
+    # than skip tests when it was important, we remove any files which we
+    # know are safe to ignore, and run tests if there's anything left.
+    changed_paths = changed_paths_from_master()
+    
+    interesting_changed_paths = [
+        c for c in changed_paths if not _is_safe_file(c)
+    ]
+    
+    if interesting_changed_paths:
+        print(
+            'Changes to the following files mean we need to run tests: %s' %
+            ', '.join(interesting_changed_paths)
+        )
+        return True
+    else:
+        print('There are no changes which would need a test run.')
+        return False
+
+
 if __name__ == '__main__':
-    subprocess.check_call(['make', os.environ['TASK']])
+    if should_run_task(task=os.environ['TASK']):
+        subprocess.check_call(['make', os.environ['TASK']])
