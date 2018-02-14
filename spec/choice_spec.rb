709a907786d245341b108c25f3c91b21175d8cfc
David R. MacIver <david@drmaciver.com>
2018-02-14 18:02:06 +0000

    Add mixed and choice_of providers

---
+++ spec/choice_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe 'choice_of provider' do
+  include Hypothesis::Debug
+
+  it 'includes the first argument' do
+    find_any do
+      m = given choice_of([0, 1])
+      m == 0
+    end
+  end
+
+  it 'includes the last argument' do
+    find_any do
+      m = given choice_of([0, 1, 2, 3])
+      m == 3
+    end
+  end
+end
