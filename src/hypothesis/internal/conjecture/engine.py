15460fe7333f582aa792bbfd04fac4b364db90c5
Kyle Reeve <krzw92@gmail.com>
2018-03-01 11:10:10 -0500

    Add more checks for state change.

--- src/hypothesis/internal/conjecture/engine.py
+++ src/hypothesis/internal/conjecture/engine.py
@@ -1550,22 +1550,17 @@ class Shrinker(object):
             # Number of blocks may have changed, need to validate
             valid_pair = [p for p in pair if p < n]
 
-            if len(valid_pair) == 0:
-                return
-
             m = min([int_from_block(p) for p in valid_pair])
-
-            if m > o:
-                return
 
             new_blocks = [self.shrink_target.buffer[u:v]
                           for u, v in self.blocks]
             for i in valid_pair:
-                new_blocks[i] = int_to_bytes(
-                    int_from_block(i) + o - m, block_len(i))
-
+                if int_from_block(i) + o - m >= 0:
+                    new_blocks[i] = int_to_bytes(
+                        int_from_block(i) + o - m, block_len(i))
             buffer = hbytes().join(new_blocks)
-            return self.incorporate_new_buffer(buffer)
+            if sort_key(buffer) < sort_key(self.shrink_target.buffer):
+                return self.incorporate_new_buffer(buffer)
 
         i = 0
         while i < len(self.blocks):
