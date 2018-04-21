063fe5cfde6c57e6a2a4bf3cd3df6c6ba699d990
David R. MacIver <david@drmaciver.com>
2018-04-21 09:35:15 +0100

    Remove weird indentation

--- build.sh
+++ build.sh
@@ -29,8 +29,8 @@ TOOL_VIRTUALENV="$VIRTUALENVS/build-$TOOL_HASH"
 TOOL_PYTHON="$TOOL_VIRTUALENV/bin/python"
 
 if [ ! -e "$TOOL_PYTHON" ] ; then
-    rm -rf "$TOOL_VIRTUALENV"
-    "$PYTHON" -m pip install --upgrade virtualenv
+  rm -rf "$TOOL_VIRTUALENV"
+  "$PYTHON" -m pip install --upgrade virtualenv
 	"$PYTHON" -m virtualenv "$TOOL_VIRTUALENV"
 	"$TOOL_PYTHON" -m pip install --no-warn-script-location -r requirements/tools.txt
 	"$TOOL_PYTHON" -m pip install -e tooling
