da6e527ba9ee5e5a248e479c2635a825a2d9beb2
David R. MacIver <david@drmaciver.com>
2018-06-18 11:10:40 +0100

    Get whole repo tests passing

--- tooling/src/hypothesistooling/projects/hypothesispython.py
+++ tooling/src/hypothesistooling/projects/hypothesispython.py
@@ -1,12 +1,31 @@
+# coding=utf-8
+#
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis-python
+#
+# Most of this work is copyright (C) 2013-2018 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# CONTRIBUTING.rst for a full list of people who may hold copyright, and
+# consult the git log if you need to determine who owns an individual
+# contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+from __future__ import division, print_function, absolute_import
+
 import os
-from hypothesistooling import git
-import hypothesistooling as tools
 import re
 import sys
-from datetime import datetime, timedelta
-import subprocess
 import shutil
+import subprocess
+from datetime import datetime, timedelta
 
+import hypothesistooling as tools
+from hypothesistooling import git
 
 PACKAGE_NAME = 'hypothesis-python'
 
@@ -35,6 +54,10 @@ with open(VERSION_FILE) as o:
 
 def has_release():
     return os.path.exists(RELEASE_FILE)
+
+
+def has_source_changes():
+    return tools.has_changes([PYTHON_SRC])
 
 
 CHANGELOG_ANCHOR = re.compile(r"^\.\. _v\d+\.\d+\.\d+:$")
