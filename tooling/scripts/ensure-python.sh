e42d09c2e42db5cd64bd32d3d35042ba8a0e14ec
David R. MacIver <david@drmaciver.com>
2018-06-29 17:40:51 +0100

    Skip retry loop

--- tooling/scripts/ensure-python.sh
+++ tooling/scripts/ensure-python.sh
@@ -81,11 +81,5 @@ if [ ! -e "$TARGET/bin/python" ] ; then
       cd "$back"
     fi
 
-    for _ in $(seq 5); do
-        if "$BASE/pyenv/plugins/python-build/bin/python-build" "$VERSION" "$TARGET" ; then
-            exit 0
-        fi
-        echo "Command failed. Retrying..."
-        sleep $(( ( RANDOM % 10 )  + 1 )).$(( RANDOM % 100 ))s
-    done
+    "$BASE/pyenv/plugins/python-build/bin/python-build" "$VERSION" "$TARGET"
 fi
