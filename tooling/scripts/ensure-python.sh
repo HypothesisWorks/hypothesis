be86637fc63cf08274fdd7eadc9a487bc5160d11
David R. MacIver <david@drmaciver.com>
2018-06-29 15:16:26 +0100

    Build shared library

--- tooling/scripts/ensure-python.sh
+++ tooling/scripts/ensure-python.sh
@@ -54,7 +54,7 @@ if [ ! -e "$TARGET/bin/python" ] ; then
         wget https://www.openssl.org/source/openssl-1.0.2o.tar.gz
         tar -xf openssl-1.0.2o.tar.gz
         cd openssl-1.0.2o
-        ./config --prefix="$OPENSSL_DIR"
+        ./config --shared --prefix="$OPENSSL_DIR"
         make install
         popd
     fi
