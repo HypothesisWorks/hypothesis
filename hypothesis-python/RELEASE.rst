acedec95a743ebc33005995fb3335cff910e51e7
Zac-HD <zac.hatfield.dodds@gmail.com>
2018-07-23 22:46:55 +1000

    Test min pytest version 3.0

---
+++ hypothesis-python/RELEASE.rst
@@ -0,0 +1,4 @@
+RELEASE_TYPE: patch
+
+This patch includes some minor fixes in the documentation, and updates
+the minimum version of :pypi:`pytest` to 3.0 (released August 2016).
--- tooling/src/hypothesistooling/__main__.py
+++ tooling/src/hypothesistooling/__main__.py
@@ -405,7 +405,7 @@ def standard_tox_task(name):
 
 
 standard_tox_task('nose')
-standard_tox_task('pytest28')
+standard_tox_task('pytest30')
 standard_tox_task('faker070')
 standard_tox_task('faker-latest')
 standard_tox_task('django20')
