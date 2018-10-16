e4dd2ba3c1712d064b588435fb2a8f3f1041a295
tyler nickerson <tylerjnickerson@gmail.com>
2018-10-15 17:22:33 -0700

    fix python2.7 compat error

--- hypothesis-python/src/hypothesis/_settings.py
+++ hypothesis-python/src/hypothesis/_settings.py
@@ -24,6 +24,7 @@ this module can be modified.
 from __future__ import division, print_function, absolute_import
 
 import os
+import types
 import warnings
 import threading
 import contextlib
@@ -223,7 +224,7 @@ class settings(
                 'settings objects can be called as a decorator with @given, '
                 'but test=%r' % (test,)
             )
-        if isinstance(test, type):
+        if isinstance(test, (type, types.ClassType)):
             from hypothesis.stateful import GenericStateMachine
             if issubclass(test, GenericStateMachine):
                 attr_name = '_hypothesis_internal_settings_applied'
