099aec65e37d6a1696d76ffcb16dd8d99269b7b2
Zac-HD <zac.hatfield.dodds@gmail.com>
2018-12-23 16:53:23 +1100

    Error on floats(inf, allow_infinity=False)

--- hypothesis-python/src/hypothesis/_strategies.py
+++ hypothesis-python/src/hypothesis/_strategies.py
@@ -534,6 +534,10 @@ def floats(
                 "Cannot have allow_infinity=%r, with both min_value and "
                 "max_value" % (allow_infinity)
             )
+    elif min_value == float("inf"):
+        raise InvalidArgument("allow_infinity=False excludes min_value=inf")
+    elif max_value == float("-inf"):
+        raise InvalidArgument("allow_infinity=False excludes max_value=-inf")
 
     def reflect_about_max(x):
         # In some cases, flipping the sign is enough to make all values valid.
