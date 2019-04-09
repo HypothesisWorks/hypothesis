726d370bafd91f22e7bf3392371d7a00d6ae94ee
Zac-HD <zac.hatfield.dodds@gmail.com>
2019-04-09 10:38:25 +1000

    Make Mypy 0.700 happy with us

--- hypothesis-python/src/hypothesis/extra/dateutil.py
+++ hypothesis-python/src/hypothesis/extra/dateutil.py
@@ -38,6 +38,16 @@ import hypothesis._strategies as st
 __all__ = ["timezones"]
 
 
+def __zone_sort_key(zone):
+    """Sort by absolute UTC offset at reference date,
+    positive first, with ties broken by name.
+    """
+    assert zone is not None
+    offset = zone.utcoffset(dt.datetime(2000, 1, 1))
+    offset = 999 if offset is None else offset
+    return (abs(offset), -offset, str(zone))
+
+
 @st.cacheable
 @st.defines_strategy
 def timezones():
@@ -52,12 +62,13 @@ def timezones():
     configuration of your machine. See the dateutil documentation for more
     information.
     """
-    reference_date = dt.datetime(2000, 1, 1)
-    tz_names = zoneinfo.get_zonefile_instance().zones
-
-    all_timezones = [tz.UTC]  # type: ignore
-    all_timezones += sorted(
-        [tz.gettz(t) for t in tz_names],
-        key=lambda zone: abs(zone.utcoffset(reference_date)),
+    all_timezones = sorted(
+        [tz.gettz(t) for t in zoneinfo.get_zonefile_instance().zones],
+        key=__zone_sort_key,
     )
-    return st.sampled_from(all_timezones)
+    all_timezones.insert(0, tz.UTC)
+    # We discard Nones in the list comprehension because Mypy knows that
+    # tz.gettz may return None.  However this should never happen for known
+    # zone names, so we assert that it's impossible first.
+    assert None not in all_timezones
+    return st.sampled_from([z for z in all_timezones if z is not None])
