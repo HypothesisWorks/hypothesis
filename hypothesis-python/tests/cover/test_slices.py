f5ea618af9283b65f7cad9cc2b52d516d6a5e1bb
Daniel West <dwest@netflix.com>
2019-05-06 16:42:49 -0400

    Missed header and additional formatting

--- hypothesis-python/tests/cover/test_slices.py
+++ hypothesis-python/tests/cover/test_slices.py
@@ -1,12 +1,30 @@
+# coding=utf-8
+#
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis/
+#
+# Most of this work is copyright (C) 2013-2019 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# CONTRIBUTING.rst for a full list of people who may hold copyright, and
+# consult the git log if you need to determine who owns an individual
+# contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+from __future__ import absolute_import, division, print_function
+
 from hypothesis import strategies as st
-from tests.common.debug import minimal, find_any, assert_all_examples
+from tests.common.debug import assert_all_examples, find_any, minimal
 
 
 def test_stop_stays_within_bounds():
     size = 1000
     assert_all_examples(
-        st.slices(size), 
-        lambda x: x.stop is None or (x.stop >= 0 and x.stop <= size)
+        st.slices(size), lambda x: x.stop is None or (x.stop >= 0 and x.stop <= size)
     )
 
 
@@ -69,4 +87,3 @@ def test_splices_will_shrink():
     assert sliced.start == 0
     assert sliced.stop == 0 or sliced.stop is None
     assert sliced.step == 1
-
