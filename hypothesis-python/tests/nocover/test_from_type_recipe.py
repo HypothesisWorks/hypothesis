acb60773bc159b3fc8e9936b454a8009a93e093b
Ryan Soklaski <ry26099@mit.edu>
2019-05-07 13:49:34 -0400

    add file header and bytes -> hbytes

--- hypothesis-python/tests/nocover/test_from_type_recipe.py
+++ hypothesis-python/tests/nocover/test_from_type_recipe.py
@@ -1,3 +1,20 @@
+# coding=utf-8
+#
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis/
+#
+# Most of this work is copyright (C) 2013-2019 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# CONTRIBUTING.rst for a full list of people who may hold copyright, and
+# consult the git log if you need to determine who owns an individual
+# contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
 from __future__ import absolute_import, division, print_function
 
 import datetime
@@ -8,6 +25,7 @@ import uuid
 
 import hypothesis.strategies as st
 from hypothesis import given
+from hypothesis.internal.compat import hbytes
 
 
 def everything_except(excluded_types):
@@ -22,7 +40,7 @@ def everything_except(excluded_types):
 many_types = [
     bool,
     bytearray,
-    bytes,
+    hbytes,
     complex,
     datetime.date,
     datetime.datetime,
