d770ea74354e12eb2117fae634c1f7ae749d1611
Ryan Soklaski <ry26099@mit.edu>
2019-05-08 19:46:11 -0400

    update docstring with Zac's edits

--- hypothesis-python/src/hypothesis/_strategies.py
+++ hypothesis-python/src/hypothesis/_strategies.py
@@ -1306,24 +1306,24 @@ def from_type(thing):
     4. Finally, if ``thing`` has type annotations for all required arguments,
        it is resolved via :func:`~hypothesis.strategies.builds`.
 
-    There is a valuable recipe for leveraging ``from_type`` to generate
+    There is a valuable recipe for leveraging ``from_type()`` to generate
     "everything except" values from a specified type. I.e.
 
-    .. code-block:: pycon
+    .. code-block:: python
 
-        >>> def everything_except(excluded_types):
-        ...     return (
-        ...         # draws from the list of registered builtin types
-        ...         from_type(type)
-        ...         # maps the drawn type to its associated strategy
-        ...         .flatmap(from_type)
-        ...         .filter(lambda x: not isinstance(x, excluded_types))
-        ...     )
+        def everything_except(excluded_types):
+            return (
+                from_type(type).flatmap(from_type)
+                .filter(lambda x: not isinstance(x, excluded_types))
+            )
 
-     For example, ``everything_except(int)`` is a strategy that will generate
-     values from all of the strategies registered with the builtin types other
-     than those that draw ``int`` values. This is useful writing tests that do
-     input validation.
+     For example, ``everything_except(int)`` returns a strategy that can
+     generate anything that ``from_type()`` can ever generate, except for
+     instances of :class:python:int, and excluding instances of types
+     added via :func:~hypothesis.strategies.register_type_strategy.
+
+     This is useful when writing tests which check that invalid input is
+     rejected in a certain way.
     """
     # TODO: We would like to move this to the top level, but pending some major
     # refactoring it's hard to do without creating circular imports.
