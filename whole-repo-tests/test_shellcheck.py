aa155f8f7a2a4adab1af86bff5e80bcc81bb910e
Zac-HD <zac.hatfield.dodds@gmail.com>
2019-06-29 17:31:50 +1000

    Ignore warning on old shellcheck

--- whole-repo-tests/test_shellcheck.py
+++ whole-repo-tests/test_shellcheck.py
@@ -27,5 +27,5 @@ SCRIPTS = [f for f in tools.all_files() if f.endswith(".sh")]
 
 def test_all_shell_scripts_are_valid():
     subprocess.check_call(
-        [install.SHELLCHECK, "--exclude=SC1073", *SCRIPTS], cwd=tools.ROOT
+        [install.SHELLCHECK, "--exclude=SC1073,SC1072", *SCRIPTS], cwd=tools.ROOT
     )
