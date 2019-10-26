cb1b6bb3d0259894992362a61b9a704cdcb022b4
Ryan Soklaski <ry26099@mit.edu>
2019-10-26 15:17:12 -0400

    fix formatting in release notes and in docstring codeblock

--- hypothesis-python/src/hypothesis/extra/numpy.py
+++ hypothesis-python/src/hypothesis/extra/numpy.py
@@ -1085,6 +1085,7 @@ def multiple_shapes(
     The following are some examples drawn from this strategy.
 
     .. code-block:: pycon
+
         >>> # Each example will draw three shapes,
         ... # and each shape is broadcast-compatible
         ... # with `(2, 3)`
