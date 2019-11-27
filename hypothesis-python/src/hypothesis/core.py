5458dcf83ca1f23ed26029b6634d1fc842ddad3e
David R. MacIver <david@drmaciver.com>
2019-11-27 10:41:01 +0000

    Reformat

--- hypothesis-python/src/hypothesis/core.py
+++ hypothesis-python/src/hypothesis/core.py
@@ -955,12 +955,7 @@ def given(
                 )
 
             state = StateForActualGivenExecution(
-                test_runner,
-                search_strategy,
-                test,
-                settings,
-                random,
-                wrapped_test,
+                test_runner, search_strategy, test, settings, random, wrapped_test,
             )
 
             reproduce_failure = wrapped_test._hypothesis_internal_use_reproduce_failure
@@ -1114,7 +1109,9 @@ def find(
     )
     if settings is None:
         settings = Settings(max_examples=2000)
-    settings = Settings(settings, suppress_health_check=HealthCheck.all(), report_multiple_bugs=False)
+    settings = Settings(
+        settings, suppress_health_check=HealthCheck.all(), report_multiple_bugs=False
+    )
 
     if database_key is None and settings.database is not None:
         database_key = function_digest(condition)
