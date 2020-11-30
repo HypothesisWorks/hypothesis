36e3a7dc32dc7d1227431bbe6f51437070e0a318
Alex Weisberger <alex.m.weisberger@gmail.com>
2020-11-29 19:45:13 -0500

    Clean up debug spec

--- hypothesis-ruby/spec/debug_spec.rb
+++ hypothesis-ruby/spec/debug_spec.rb
@@ -3,20 +3,20 @@
 RSpec.describe 'find' do
   include Hypothesis::Debug
 
-  # it "raises an error if it can't find anything" do
-  #   expect do
-  #     find do
-  #       any integers
-  #       false
-  #     end
-  #   end.to raise_exception(Hypothesis::Debug::NoSuchExample)
-  # end
+  it "raises an error if it can't find anything" do
+    expect do
+      find do
+        any integers
+        false
+      end
+    end.to raise_exception(Hypothesis::Debug::NoSuchExample)
+  end
 
-  it do
+  it 'Rutie binding tests' do
     hypothesis do
+      x = any(integers)
+      puts x
       puts any(arrays(of: integers)).to_s
-#      puts "Debuggin"
-#      puts any integers
     end
   end
 end
