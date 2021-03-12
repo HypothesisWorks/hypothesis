88426e5c73a06effa6020d5482093302fae2b8ff
Zac-HD <zac.hatfield.dodds@gmail.com>
2021-03-12 11:46:53 +1100

    Fix Actions install+caching
    Only TOOL_PYTHON (currently 3.8) should be provided by Actions; the others are installed by pyenv as needed.
    
    This was mostly harmless since we only actually *used* the tool venv on 3.8 tasks, but still...

--- tooling/src/hypothesistooling/__main__.py
+++ tooling/src/hypothesistooling/__main__.py
@@ -310,7 +310,7 @@ def run_tox(task, version):
 # Via https://github.com/pyenv/pyenv/tree/master/plugins/python-build/share/python-build
 PY36 = "3.6.13"
 PY37 = "3.7.10"
-PY38 = PYMAIN = "3.8.8"  # Note: keep this in sync with build.sh
+PY38 = PYMAIN = "3.8.8"  # Note: keep this in sync with build.sh and GH Actions main.yml
 PY39 = "3.9.2"
 PYPY36 = "pypy3.6-7.3.1"
 PYPY37 = "pypy3.7-7.3.2"
