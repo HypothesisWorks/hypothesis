2756b768d3e227fbeea130f71fe4838057b56a00
CI on behalf of the Hypothesis team <david@drmaciver.com>
2021-09-05 00:06:45 +0000

    Update pinned dependencies

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ if [ -n "${GITHUB_ACTIONS-}" ] || [ -n "${CODESPACES-}" ] ; then
 else
     # Otherwise, we install it from scratch
     # NOTE: tooling keeps this version in sync with PYMAIN in tooling
-    "$SCRIPTS/ensure-python.sh" 3.8.11
-    PYTHON=$(pythonloc 3.8.11)/bin/python
+    "$SCRIPTS/ensure-python.sh" 3.8.12
+    PYTHON=$(pythonloc 3.8.12)/bin/python
 fi
 
 TOOL_REQUIREMENTS="$ROOT/requirements/tools.txt"
