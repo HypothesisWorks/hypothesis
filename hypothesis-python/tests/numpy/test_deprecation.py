f02870b7fcd42830d30173c39b0f6b7e5fb9b9e7
Matthew Barber <quitesimplymatt@gmail.com>
2021-09-14 13:37:00 +0100

    Test for deprecation

---
+++ hypothesis-python/tests/numpy/test_deprecation.py
@@ -0,0 +1,24 @@
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis/
+#
+# Most of this work is copyright (C) 2013-2021 David R. MacIver
+# (david@drmaciver.com), but it contains contributions by others. See
+# CONTRIBUTING.rst for a full list of people who may hold copyright, and
+# consult the git log if you need to determine who owns an individual
+# contribution.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+#
+# END HEADER
+
+import pytest
+
+from hypothesis.errors import HypothesisDeprecationWarning
+from hypothesis.extra import numpy as nps
+
+
+def test_basic_indices_bad_max_dims_warns():
+    with pytest.warns(HypothesisDeprecationWarning):
+        nps.basic_indices((3, 3, 3), max_dims=4).example()
