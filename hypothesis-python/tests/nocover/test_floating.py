67908b22841c51a22d7c5cd485a99f2b0aaa3ed3
Matthew <quitesimplymatt@gmail.com>
2021-11-19 13:06:26 +0000

    Add `allow_subnormal` kwarg to `st.floats()`

--- hypothesis-python/tests/nocover/test_floating.py
+++ hypothesis-python/tests/nocover/test_floating.py
@@ -16,16 +16,18 @@
 """Tests for being able to generate weird and wonderful floating point numbers."""
 
 import math
-import sys
+from itertools import product
+from sys import float_info
 
 import pytest
+from tests.common.debug import assert_all_examples, assert_no_examples, find_any
+from tests.common.utils import fails
 
 from hypothesis import HealthCheck, assume, given, settings
 from hypothesis.internal.floats import float_to_int, next_down
+from hypothesis.errors import InvalidArgument
+from hypothesis.internal.floats import next_down, next_up
 from hypothesis.strategies import data, floats, lists
-
-from tests.common.debug import find_any
-from tests.common.utils import fails
 
 TRY_HARDER = settings(
     max_examples=1000, suppress_health_check=[HealthCheck.filter_too_much]
@@ -47,7 +49,7 @@ def test_inversion_is_imperfect(x):
     assert x * y == 1.0
 
 
-@given(floats(-sys.float_info.max, sys.float_info.max))
+@given(floats(-float_info.max, float_info.max))
 def test_largest_range(x):
     assert not math.isinf(x)
 
@@ -111,7 +113,7 @@ def test_is_in_exact_int_range(x):
 # compiler options like `-ffast-math`.  On most hardware that's even a global
 # config option, so *linking against* something built this way can break us.
 # Everything is terrible
-FLUSH_SUBNORMALS_TO_ZERO = next_down(sys.float_info.min) == 0.0
+FLUSH_SUBNORMALS_TO_ZERO = next_down(float_info.min) == 0.0
 
 
 def test_compiled_with_sane_math_options():
@@ -125,7 +127,7 @@ def test_compiled_with_sane_math_options():
 @TRY_HARDER
 def test_can_generate_really_small_positive_floats(x):
     assume(x > 0)
-    assert x >= sys.float_info.min
+    assert x >= float_info.min
 
 
 @pytest.mark.skipif(FLUSH_SUBNORMALS_TO_ZERO, reason="broken by unsafe compiler flags")
@@ -134,7 +136,104 @@ def test_can_generate_really_small_positive_floats(x):
 @TRY_HARDER
 def test_can_generate_really_small_negative_floats(x):
     assume(x < 0)
-    assert x <= -sys.float_info.min
+    assert x <= -float_info.min
+
+
+@pytest.mark.parametrize(
+    "min_value, max_value",
+    [
+        (None, None),
+        (-1, 0),
+        (0, 1),
+        (-1, 1),
+    ],
+)
+@pytest.mark.parametrize(
+    "width, smallest_normal",
+    [(16, 2 ** -14), (32, 2 ** -126), (64, 2 ** -1022)],
+    ids=["16", "32", "64"],
+)
+def test_does_not_generate_subnormals_when_disallowed(
+    width,
+    smallest_normal,
+    min_value,
+    max_value,
+):
+    strat = floats(
+        min_value=min_value,
+        max_value=max_value,
+        allow_subnormal=False,
+        width=width,
+    )
+    strat = strat.filter(lambda x: x != 0.0 and not math.isnan(x) and not math.isinf(x))
+    assert_all_examples(strat, lambda x: x <= -smallest_normal or x >= smallest_normal)
+
+
+def kw(**kwargs):
+    id_ = ", ".join(f"{k}={v!r}" for k, v in kwargs.items())
+    return pytest.param(kwargs, id=id_)
+
+
+@pytest.mark.skipif(FLUSH_SUBNORMALS_TO_ZERO, reason="broken by unsafe compiler flags")
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        kw(min_value=1),
+        kw(min_value=1),
+        kw(max_value=-1),
+        kw(min_value=float_info.min),
+        kw(min_value=next_down(float_info.min), exclude_min=True),
+        kw(max_value=-float_info.min),
+        kw(min_value=next_up(-float_info.min), exclude_max=True),
+    ],
+)
+def test_subnormal_validation(kwargs):
+    strat = floats(**kwargs, allow_subnormal=True)
+    with pytest.raises(InvalidArgument):
+        strat.example()
+
+
+@pytest.mark.skipif(FLUSH_SUBNORMALS_TO_ZERO, reason="broken by unsafe compiler flags")
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        # min value
+        kw(allow_subnormal=False, min_value=1),
+        kw(allow_subnormal=False, min_value=float_info.min),
+        kw(allow_subnormal=True, min_value=-1),
+        kw(allow_subnormal=True, min_value=next_down(float_info.min)),
+        # max value
+        kw(allow_subnormal=False, max_value=-1),
+        kw(allow_subnormal=False, max_value=-float_info.min),
+        kw(allow_subnormal=True, max_value=1),
+        kw(allow_subnormal=True, max_value=next_up(-float_info.min)),
+        # min/max values
+        kw(allow_subnormal=True, min_value=-1, max_value=1),
+        kw(
+            allow_subnormal=True,
+            min_value=next_down(float_info.min),
+            max_value=float_info.min,
+        ),
+        kw(
+            allow_subnormal=True,
+            min_value=-float_info.min,
+            max_value=next_up(-float_info.min),
+        ),
+        kw(allow_subnormal=False, min_value=-1, max_value=-float_info.min),
+        kw(allow_subnormal=False, min_value=float_info.min, max_value=1),
+    ],
+)
+def test_allow_subnormal_defaults_correctly(kwargs):
+    allow_subnormal = kwargs["allow_subnormal"]
+    del kwargs["allow_subnormal"]
+    strat = floats(**kwargs).filter(lambda x: x != 0)
+    if allow_subnormal:
+        find_any(
+            strat,
+            lambda x: -float_info.min < x < float_info.min,
+        )
+    else:
+        assert_no_examples(strat, lambda x: -float_info.min < x < float_info.min)
 
 
 @fails
