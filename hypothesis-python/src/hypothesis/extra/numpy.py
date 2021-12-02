3e1e87a36dafbddcc231487b9340fc6d174611cb
Matthew <quitesimplymatt@gmail.com>
2021-12-02 07:40:53 +0000

    Raise specific error for subnormal scenarios in `nps.arrays()`

--- hypothesis-python/src/hypothesis/extra/numpy.py
+++ hypothesis-python/src/hypothesis/extra/numpy.py
@@ -204,6 +204,21 @@ class ArrayStrategy(st.SearchStrategy):
             ) from err
         if self._check_elements and val != result[idx] and val == val:
             strategy = self.fill if fill else self.element_strategy
+            if self.dtype.kind == "f":
+                try:
+                    is_subnormal = 0 < abs(val) < np.finfo(self.dtype).tiny
+                except Exception:  # pragma: no cover
+                    # val may be a non-float that does not support the
+                    # operations __lt__ and __abs__
+                    is_subnormal = False  # pragma: no cover
+                if is_subnormal:
+                    raise InvalidArgument(  # pragma: no cover
+                        f"Generated subnormal float {val} from strategy "
+                        f"{strategy} resulted in {result[idx]!r}, probably "
+                        "as a result of NumPy being built with flush-to-zero "
+                        "compiler options. Consider passing "
+                        "allow_subnormal=False."
+                    )
             raise InvalidArgument(
                 "Generated array element %r from %r cannot be represented as "
                 "dtype %r - instead it becomes %r (type %r).  Consider using a more "
