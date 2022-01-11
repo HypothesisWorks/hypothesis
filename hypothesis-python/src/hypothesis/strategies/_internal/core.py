ebf158c1d63d935014de7007a1fbb72897b69c30
Gabe Joseph <gjoseph92@gmail.com>
2022-01-10 18:45:19 -0700

    TypeVar for return type of `functions`
    Probably won't work, but let's see

--- hypothesis-python/src/hypothesis/strategies/_internal/core.py
+++ hypothesis-python/src/hypothesis/strategies/_internal/core.py
@@ -1854,13 +1854,16 @@ def emails() -> SearchStrategy[str]:
 
 
 P = ParamSpec("P")
+T = TypeVar("T")
+
+
 @defines_strategy()
 def functions(
     *,
-    like: Callable[P, Any] = lambda: None,
-    returns: Optional[SearchStrategy[Any]] = None,
+    like: Callable[P, T] = lambda: None,
+    returns: Optional[SearchStrategy[T]] = None,
     pure: bool = False,
-) -> SearchStrategy[Callable[P, Any]]:
+) -> SearchStrategy[Callable[P, T]]:
     # The proper type signature of `functions()` would have T instead of Any, but mypy
     # disallows default args for generics: https://github.com/python/mypy/issues/3737
     """functions(*, like=lambda: None, returns=none(), pure=False)
