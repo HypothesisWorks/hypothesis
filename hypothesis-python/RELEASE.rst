2e57d1e8aa2a1c74149a871a6d829d6bd921a6dc
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2022-04-09 21:00:45 -0700

    Tweak junit-xml test config

--- hypothesis-python/RELEASE.rst
+++ hypothesis-python/RELEASE.rst
@@ -1,12 +1,16 @@
 RELEASE_TYPE: patch
 
-The Hypothesis pytest plugin was not outputting valid xunit2 nodes when ``--junit-xml`` was specified. This has been broken since Pytest 5.4, which changed the internal API for adding nodes to the junit report.
+The Hypothesis pytest plugin was not outputting valid xunit2 nodes when
+``--junit-xml`` was specified. This has been broken since Pytest 5.4, which
+changed the internal API for adding nodes to the junit report.
 
-This also fixes the issue when using hypothesis with ``--junit-xml`` and ``pytest-xdist`` where the junit xml report would not be xunit2 compatible. Now, when using with ``pytest-xdist``, the junit report will just omit the ``<properties>`` node.
+This also fixes the issue when using hypothesis with ``--junit-xml`` and
+``pytest-xdist`` where the junit xml report would not be xunit2 compatible.
+Now, when using with ``pytest-xdist``, the junit report will just omit the
+``<properties>`` node.
 
-References:
-* https://github.com/pytest-dev/pytest/issues/7767#issuecomment-1082436256
-* https://github.com/pytest-dev/pytest/issues/1126#issuecomment-484581283
-* :issue:`1935`
+For more details, see `this pytest issue <https://github.com/pytest-dev/pytest/issues/1126#issuecomment-484581283>`__,
+`this pytest issue <https://github.com/pytest-dev/pytest/issues/7767#issuecomment-1082436256>`__,
+and :issue:`1935`
 
 Thanks to Brandon Chinn for this bug fix!
--- hypothesis-python/tests/pytest/test_junit.py
+++ hypothesis-python/tests/pytest/test_junit.py
@@ -37,28 +37,37 @@ def _run_and_get_junit(testdir, *args):
 def _findall_from_root(junit_xml, path):
     if junit_xml.tag == "testsuites":
         return junit_xml.findall(f"./testsuite/{path}")
-    else:
-        # < pytest 5.1.0
-        # https://github.com/pytest-dev/pytest/commit/a43ba78d3bde1630a42aaa95776687d3886891e6
-        return junit_xml.findall(f"./{path}")
+    # This case only exists for tests against Pytest before 5.1.0;
+    # see https://github.com/pytest-dev/pytest/commit/a43ba78d3bde
+    return junit_xml.findall(f"./{path}")
 
 
-def test_outputs_valid_xunit2(testdir):
-    junit_xml = _run_and_get_junit(testdir)
-
+def suite_properties_ok(junit_xml):
+    # Check whether <properties> is included in <testsuite>.  This is currently not
+    # the case when using pytest-xdist, which is a shame, but we can live with it.
     testsuite_props = _findall_from_root(junit_xml, "properties")
-    assert len(testsuite_props) == 1
-    assert {prop.get("name") for prop in testsuite_props[0].findall("property")} == {
+    return len(testsuite_props) == 1 and {
+        prop.get("name") for prop in testsuite_props[0].findall("property")
+    } == {
         "hypothesis-statistics-test_outputs_valid_xunit2.py::test_valid",
         "hypothesis-statistics-test_outputs_valid_xunit2.py::test_invalid",
     }
 
+
+def test_outputs_valid_xunit2(testdir):
+    # The thing we really care about with pytest-xdist + junitxml is that we don't
+    # break xunit2 compatibility by putting <properties> inside <testcase>.
+    junit_xml = _run_and_get_junit(testdir)
     testcase_props = _findall_from_root(junit_xml, "testcase/properties")
     assert len(testcase_props) == 0
+    # Check whether <properties> is included in <testsuite>
+    assert suite_properties_ok(junit_xml)
 
 
 def test_outputs_valid_xunit2_with_xdist(testdir):
     junit_xml = _run_and_get_junit(testdir, "-n2")
-
     testcase_props = _findall_from_root(junit_xml, "testcase/properties")
     assert len(testcase_props) == 0
+    # If <properties> is included in <testsuite>, this assertion will fail.
+    # That would be a GOOD THING, and we would remove the `not` to prevent regressions.
+    assert not suite_properties_ok(junit_xml)
