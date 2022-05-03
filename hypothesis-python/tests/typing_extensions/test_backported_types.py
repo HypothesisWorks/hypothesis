f7ae5c1129c5404fef41327da05831d945f5bcc2
Cheukting <cheukting.ho@gmail.com>
2022-05-02 22:41:09 -0600

    skip callable arg check for 3.7 or below

--- hypothesis-python/tests/typing_extensions/test_backported_types.py
+++ hypothesis-python/tests/typing_extensions/test_backported_types.py
@@ -231,6 +231,7 @@ def test_non_runtime_type_cannot_be_registered(non_runtime_type):
         st.register_type_strategy(non_runtime_type, st.none())
 
 
+@pytest.mark.skipif(sys.version_info <= (3, 7), reason="requires python3.8 or higher")
 @pytest.mark.parametrize("non_runtime_type", [Concatenate, ParamSpec])
 def test_callable_with_non_runtime_type(non_runtime_type):
     strategy = st.from_type(Callable[non_runtime_type])
