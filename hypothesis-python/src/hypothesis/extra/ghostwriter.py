158bb6ea5ad38db9518fc55b2d0eaf3f21155943
Cheukting <cheukting.ho@gmail.com>
2022-05-31 08:10:28 +0100

    update type annotation

--- hypothesis-python/src/hypothesis/extra/ghostwriter.py
+++ hypothesis-python/src/hypothesis/extra/ghostwriter.py
@@ -892,7 +892,7 @@ def magic(
     for thing in modules_or_functions:
         if callable(thing):
             functions.add(thing)
-            funcs: List[Callable] = []
+            funcs: list = []
         elif isinstance(thing, types.ModuleType):
             if hasattr(thing, "__all__"):
                 funcs = [getattr(thing, name, None) for name in thing.__all__]
