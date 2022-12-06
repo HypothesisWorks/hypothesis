032375e3d0537acb0401eb0e32103180c93d8059
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2022-12-06 01:32:41 -0800

    Tidy up nocover pragmas

--- hypothesis-python/src/hypothesis/extra/ghostwriter.py
+++ hypothesis-python/src/hypothesis/extra/ghostwriter.py
@@ -117,7 +117,7 @@ from hypothesis.strategies._internal.strategies import (
 )
 from hypothesis.strategies._internal.types import _global_type_lookup, is_generic_type
 
-if sys.version_info >= (3, 10):  # pragma: no cover
+if sys.version_info >= (3, 10):
     from types import EllipsisType as EllipsisType
 elif TYPE_CHECKING:
     from builtins import ellipsis as EllipsisType
