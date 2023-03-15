1f4c7170a171b5938f3792c36b47b8c3bff1f430
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2023-03-14 23:25:47 -0700

    Mock pytz for docs

--- hypothesis-python/docs/conf.py
+++ hypothesis-python/docs/conf.py
@@ -106,7 +106,7 @@ intersphinx_mapping = {
     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 
-autodoc_mock_imports = ["numpy", "pandas", "redis", "django"]
+autodoc_mock_imports = ["numpy", "pandas", "redis", "django", "pytz"]
 
 codeautolink_autodoc_inject = False
 codeautolink_global_preface = """
