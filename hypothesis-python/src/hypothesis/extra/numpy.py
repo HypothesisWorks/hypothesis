f33ae9cd80cfcff3005b519f5f5d25ec20f8648d
Francesc Elies <francesc.elies@mbbm-ast.com>
2023-03-28 12:05:28 +0200

    [TYP] strategy parameters type are related to each other

--- hypothesis-python/src/hypothesis/extra/numpy.py
+++ hypothesis-python/src/hypothesis/extra/numpy.py
@@ -383,8 +383,8 @@ def arrays(
     dtype: D,
     shape: Union[int, st.SearchStrategy[int], Shape, st.SearchStrategy[Shape]],
     *,
-    elements: Optional[Union[st.SearchStrategy, Mapping[str, Any]]] = None,
-    fill: Optional[st.SearchStrategy[Any]] = None,
+    elements: Optional[Union[st.SearchStrategy[D], Mapping[str, Any]]] = None,
+    fill: Optional[st.SearchStrategy[D]] = None,
     unique: bool = False,
 ) -> st.SearchStrategy[NDArray[D]]:
     r"""Returns a strategy for generating :class:`numpy:numpy.ndarray`\ s.
