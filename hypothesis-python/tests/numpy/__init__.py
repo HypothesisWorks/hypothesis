39851138cc859846b8a20abf683b874fc8ab0462
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2023-07-04 15:23:12 -0700

    Work around Numpy encoding warning

--- hypothesis-python/tests/numpy/__init__.py
+++ hypothesis-python/tests/numpy/__init__.py
@@ -7,3 +7,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
+
+try:
+    EncodingWarning
+except NameError:
+    pass
+else:
+    # Work around https://github.com/numpy/numpy/issues/24115
+    import warnings
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", EncodingWarning)
+        import numpy.testing  # noqa
