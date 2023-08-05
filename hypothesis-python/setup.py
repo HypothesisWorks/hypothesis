54fab2c7254c0bca18fd667fee8ea4167b92270e
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2023-08-05 16:34:36 -0700

    Fix build on Python 3.8
    See issue 3707.

--- hypothesis-python/setup.py
+++ hypothesis-python/setup.py
@@ -25,7 +25,7 @@ if sys.version_info[:2] < (3, 8):
 
 
 def local_file(name):
-    return Path(__file__).parent.joinpath(name).relative_to(Path.cwd())
+    return Path(__file__).absolute().parent.joinpath(name).relative_to(Path.cwd())
 
 
 SOURCE = str(local_file("src"))
