609c64be4ca1e343d0f761d49629b69c771006ee
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2023-09-10 13:39:04 -0700

    Fix parametrize false-alarm healthchecks

--- hypothesis-python/tests/pytest/test_parametrized_db_keys.py
+++ hypothesis-python/tests/pytest/test_parametrized_db_keys.py
@@ -8,6 +8,10 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
 
+import pytest
+
+from hypothesis import HealthCheck, given, settings, strategies as st
+
 DB_KEY_TESTCASE = """
 from hypothesis import settings, given
 from hypothesis.database import InMemoryExampleDatabase
@@ -33,3 +37,22 @@ def test_DB_keys_for_parametrized_test():
 def test_db_keys_for_parametrized_tests_are_unique(testdir):
     script = testdir.makepyfile(DB_KEY_TESTCASE)
     testdir.runpytest(script).assert_outcomes(xfailed=3, passed=1)
+
+
+@pytest.fixture(params=["a", "b"])
+def fixt(request):
+    return request.param
+
+
+class TestNoDifferingExecutorsHealthCheck:
+    # Regression test for https://github.com/HypothesisWorks/hypothesis/issues/3733
+
+    @given(x=st.text())
+    @pytest.mark.parametrize("i", range(2))
+    def test_method(self, x, i):
+        pass
+
+    @settings(suppress_health_check=[HealthCheck.function_scoped_fixture])
+    @given(x=st.text())
+    def test_method_fixture(self, x, fixt):
+        pass
