7dc5a1d18e092003f16aa2f065951615dfad57d5
Viicos <65306057+Viicos@users.noreply.github.com>
2023-11-01 21:02:25 +0100

    Handle annotated types as filter, handle timezones

--- hypothesis-python/src/hypothesis/strategies/_internal/annotated_types.py
+++ hypothesis-python/src/hypothesis/strategies/_internal/annotated_types.py
@@ -1,42 +1,102 @@
 import operator
-from typing import Any, Tuple, Type, TypeVar, Iterator, Callable, List
+from datetime import datetime, time, timezone
 from functools import partial
+from typing import Any, Callable, Iterator, List, Protocol, Tuple, Type, TypeVar
+
+import annotated_types as at
+
+import hypothesis.strategies as st
+from hypothesis.errors import InvalidArgument
 
 try:
-    import annotated_types
+    import zoneinfo
 except ImportError:
-    annotated_types = None
+    try:
+        from backports import zoneinfo  # type: ignore
+    except ImportError:
+        # We raise an error recommending `pip install hypothesis[zoneinfo]`
+        # when at.Timezone is used.
+        zoneinfo = None  # type: ignore
 
-from hypothesis.strategies import from_type
 
-T = TypeVar("T")
+Ex = TypeVar("Ex", covariant=True)
 
 
-def get_constraints(args: Tuple[Any, ...]) -> Iterator[annotated_types.BaseMetadata]:
+class SupportsLen(Protocol):
+    def __len__(self) -> int:
+        ...
+
+
+def max_len(element: SupportsLen, max_length: int) -> bool:
+    return len(element) <= max_length
+
+
+def min_len(element: SupportsLen, min_length: int) -> bool:
+    return len(element) >= min_length
+
+
+CONSTRAINTS_FILTER_MAP = {
+    # Due to the order of operator.gt/ge/lt/le arguments, order is inversed:
+    at.Gt: lambda constraint: partial(operator.lt, constraint.gt),
+    at.Ge: lambda constraint: partial(operator.le, constraint.ge),
+    at.Lt: lambda constraint: partial(operator.gt, constraint.lt),
+    at.Le: lambda constraint: partial(operator.ge, constraint.le),
+    at.MaxLen: lambda constraint: partial(max_len, max_length=constraint.max_length),
+    at.MinLen: lambda constraint: partial(min_len, max_length=constraint.min_length),
+    at.Predicate: lambda constraint: constraint.func,
+}
+
+
+def _get_constraints(args: Tuple[Any, ...]) -> Iterator[at.BaseMetadata]:
     for arg in args:
-        if isinstance(arg, annotated_types.BaseMetadata):
+        if isinstance(arg, at.BaseMetadata):
             yield arg
         elif getattr(arg, "__is_annotated_types_grouped_metadata__", False):
             yield from arg
         elif isinstance(arg, slice):
-            yield from annotated_types.Len(arg.start or 0, arg.stop)
+            yield from at.Len(arg.start or 0, arg.stop)
 
 
-def from_annotated_types(type_: Type[T], args: Tuple[Any, ...]):
-    if annotated_types is None:
-        return None
+def _infer_strategy(type_: Type[Ex], constraints: List[at.BaseMetadata]) -> st.SearchStrategy[Ex]:
+    tz_constraint = next((c for c in constraints if isinstance(c, at.Timezone)), None)
+    if tz_constraint is not None:
+        if type_ not in [datetime, time]:
+            raise InvalidArgument(f"Annotated type {type_} is not applicable with constraint {tz_constraint}")
 
-    base_strategy = from_type(type_)
+        constructor = st.datetimes if type_ is datetime else st.times
+        tz_attr = tz_constraint.tz
+
+        if tz_attr is None:
+            return constructor()
+        if tz_attr is ...:
+            return constructor(timezones=st.timezones())
+        if isinstance(tz_attr, timezone):
+            return constructor(timezones=st.just(tz_attr))
+        if isinstance(tz_attr, str):
+            if zoneinfo is None:
+                # TODO this is duplicated from the st.timezones body, should be refactored:
+                raise ModuleNotFoundError(
+                    "The zoneinfo module is required, but could not be imported.  "
+                    "Run `pip install hypothesis[zoneinfo]` and try again."
+                )
+            return constructor(timezones=st.just(zoneinfo.ZoneInfo(tz_attr)))
+        else:
+            raise InvalidArgument(f"Unknown argument for annotated_types.Timezone: {tz_attr!r}")
+
+    return st.from_type(type_)
+
+
+def from_annotated_types(type_: Type[Ex], args: Tuple[Any, ...]) -> st.SearchStrategy[Ex]:
+    constraints = list(_get_constraints(args))
+    base_strategy = _infer_strategy(type_, constraints)
 
     filter_conditions: List[Callable[[Any], Any]] = []
-    for constraint in get_constraints(args):
-        if isinstance(constraint, annotated_types.Gt):
-            filter_conditions.append(partial(operator.gt, constraint.gt))
-        if isinstance(constraint, annotated_types.Ge):
-            filter_conditions.append(partial(operator.ge, constraint.ge))
-        if isinstance(constraint, annotated_types.Lt):
-            filter_conditions.append(partial(operator.lt, constraint.lt))
-        if isinstance(constraint, annotated_types.Le):
-            filter_conditions.append(partial(operator.le, constraint.le))
-        if isinstance(constraint, annotated_types.Predicate):
-            filter_conditions.append(constraint.func)
+    for constraint in constraints:
+        if type(constraint) in CONSTRAINTS_FILTER_MAP:
+            condition = CONSTRAINTS_FILTER_MAP[type(constraint)]
+            filter_conditions.append(condition(constraint))
+
+    for filter_condition in filter_conditions:
+        base_strategy = base_strategy.filter(filter_condition)
+
+    return base_strategy
