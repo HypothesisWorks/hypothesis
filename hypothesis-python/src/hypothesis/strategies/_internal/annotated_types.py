5607d343bf60e1f1cc95899726a17bac771ab693
Viicos <65306057+Viicos@users.noreply.github.com>
2023-11-04 15:02:16 +0100

    Remove support for `at.Timezone`

--- hypothesis-python/src/hypothesis/strategies/_internal/annotated_types.py
+++ hypothesis-python/src/hypothesis/strategies/_internal/annotated_types.py
@@ -1,24 +1,22 @@
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis/
+#
+# Copyright the Hypothesis Authors.
+# Individual contributors are listed in AUTHORS.rst and the git log.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
 import operator
 import warnings
-from datetime import datetime, time, timezone
 from functools import partial
 from typing import Any, Callable, Iterator, List, Protocol, Tuple, Type, TypeVar
 
 import annotated_types as at
 
 import hypothesis.strategies as st
-from hypothesis.errors import InvalidArgument
 from hypothesis.reporting import debug_report
-
-try:
-    import zoneinfo
-except ImportError:
-    try:
-        from backports import zoneinfo  # type: ignore
-    except ImportError:
-        # We raise an error recommending `pip install hypothesis[zoneinfo]`
-        # when at.Timezone is used.
-        zoneinfo = None  # type: ignore
 
 
 Ex = TypeVar("Ex", covariant=True)
@@ -59,60 +57,28 @@ def _get_constraints(args: Tuple[Any, ...]) -> Iterator[at.BaseMetadata]:
             yield from at.Len(arg.start or 0, arg.stop)
 
 
-def _infer_strategy(
-    type_: Type[Ex], constraints: List[at.BaseMetadata]
-) -> st.SearchStrategy[Ex]:
-    tz_constraint = next((c for c in constraints if isinstance(c, at.Timezone)), None)
-    if tz_constraint is not None:
-        if type_ not in [datetime, time]:
-            raise InvalidArgument(
-                f"Annotated type {type_} is not applicable with constraint {tz_constraint}"
-            )
-
-        constraints.remove(tz_constraint)
-        constructor = st.datetimes if type_ is datetime else st.times
-        tz_attr = tz_constraint.tz
-
-        if tz_attr is None:
-            return constructor()
-        if tz_attr is ...:
-            return constructor(timezones=st.timezones())
-        if isinstance(tz_attr, timezone):
-            return constructor(timezones=st.just(tz_attr))
-        if isinstance(tz_attr, str):
-            if zoneinfo is None:
-                # TODO this is duplicated from the st.timezones body, should be refactored:
-                raise ModuleNotFoundError(
-                    "The zoneinfo module is required, but could not be imported.  "
-                    "Run `pip install hypothesis[zoneinfo]` and try again."
-                )
-            return constructor(timezones=st.just(zoneinfo.ZoneInfo(tz_attr)))
-        else:
-            raise InvalidArgument(
-                f"Unknown argument for annotated_types.Timezone: {tz_attr!r}"
-            )
-
-    return st.from_type(type_)
-
-
 def from_annotated_types(
     type_: Type[Ex], args: Tuple[Any, ...]
 ) -> st.SearchStrategy[Ex]:
     # `constraints` elements can be "consumed" by the next following calls/instructions
     constraints = list(_get_constraints(args))
 
-    multipleof_constraint = next(
-        (c for c in constraints if isinstance(c, at.MultipleOf)), None
-    )
-    if multipleof_constraint is not None:
+    unsupported_constraints = [
+        c for c in constraints if isinstance(c, (at.MultipleOf, at.Timezone))
+    ]
+
+    if unsupported_constraints:
         warnings.warn(
-            f"{multipleof_constraint} is currently not supported and will be ignored.",
+            f"{', '.join(unsupported_constraints)} ",
+            f"{'is' if len(unsupported_constraints) == 1 else 'are'} ",
+            "currently not supported and will be ignored.",
             UserWarning,
             stacklevel=2,
         )
-        constraints.remove(multipleof_constraint)
+        for c in unsupported_constraints:
+            constraints.remove(c)
 
-    base_strategy = _infer_strategy(type_, constraints)
+    base_strategy = st.from_type(type_)
 
     filter_conditions: List[Callable[[Any], Any]] = []
     for constraint in constraints:
