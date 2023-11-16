ab973c77208a26b72fa6999c0c8c3e68c8dc1074
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2023-11-16 00:58:26 -0500

    Update test_text.py

--- hypothesis-python/tests/cover/test_text.py
+++ hypothesis-python/tests/cover/test_text.py
@@ -8,21 +8,11 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
 
-from hypothesis.internal.conjecture.junkdrawer import (
-    Z_point,
-    char_rewrite_integer,
-    zero_point,
-)
 from hypothesis.strategies._internal.strings import OneCharStringStrategy
 
 
 def test_rewriting_integers_covers_right_range():
     strategy = OneCharStringStrategy.from_characters_args()
-
-    zero_point_ = zero_point(strategy.intervals)
-    Z_point_ = Z_point(strategy.intervals)
-    rewritten = [
-        char_rewrite_integer(i, zero_point=zero_point_, Z_point=Z_point_)
-        for i in range(256)
-    ]
+    rewritten = [ord(strategy.intervals.char_in_shrink_order(i)) for i in range(256)]
+    assert rewritten != list(range(256))
     assert sorted(rewritten) == sorted(range(256))
