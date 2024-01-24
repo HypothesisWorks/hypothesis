08ad35fd0da746e6654b598706042bb9c18f52a0
Joachim B Haga <jobh@simula.no>
2024-01-24 12:53:37 +0100

    Use non-mapped strategies for populating numpy arrays

--- hypothesis-python/src/hypothesis/extra/numpy.py
+++ hypothesis-python/src/hypothesis/extra/numpy.py
@@ -46,8 +46,14 @@ from hypothesis.internal.conjecture import utils as cu
 from hypothesis.internal.coverage import check_function
 from hypothesis.internal.reflection import proxies
 from hypothesis.internal.validation import check_type
+from hypothesis.strategies._internal.lazy import unwrap_strategies
 from hypothesis.strategies._internal.numbers import Real
-from hypothesis.strategies._internal.strategies import Ex, T, check_strategy
+from hypothesis.strategies._internal.strategies import (
+    Ex,
+    MappedSearchStrategy,
+    T,
+    check_strategy,
+)
 from hypothesis.strategies._internal.utils import defines_strategy
 
 
@@ -507,6 +513,13 @@ def arrays(
                 .flatmap(lambda d: arrays(d, shape=shape, fill=fill, unique=unique))
             )
         elements = from_dtype(dtype, **(elements or {}))
+        # from_dtype may return a MappedStrategy to return values of the correct dtype. Since
+        # we're populating dtyped array elements with it, this is redundant. Worse, it's
+        # actively bad since it may preclude certain optimizations like fast unique sampling.
+        # So: just prune away the mapping.
+        elements_unwrapped = unwrap_strategies(elements)
+        if isinstance(elements_unwrapped, MappedSearchStrategy):
+            elements = elements_unwrapped.mapped_strategy
     check_strategy(elements, "elements")
     if isinstance(shape, int):
         shape = (shape,)
