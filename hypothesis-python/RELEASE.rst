5428710cfab9b83e82f8c9bd8dbd34fe142afed6
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2024-02-22 00:33:06 -0800

    Add note to errors from strategies

---
+++ hypothesis-python/RELEASE.rst
@@ -0,0 +1,4 @@
+RELEASE_TYPE: patch
+
+This patch :pep:`adds a note <678>` to errors which occur while drawing from
+a strategy, to make it easier to tell why your test failed in such cases.
--- hypothesis-python/tests/cover/test_error_in_draw.py
+++ hypothesis-python/tests/cover/test_error_in_draw.py
@@ -8,10 +8,13 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
 
+import re
+
 import pytest
 
 from hypothesis import given, strategies as st
 from hypothesis.errors import HypothesisWarning
+from hypothesis.stateful import RuleBasedStateMachine, rule, run_state_machine_as_test
 
 
 def test_error_is_in_finally():
@@ -36,3 +39,31 @@ def test_warns_on_bool_strategy(data):
     ):
         if st.booleans():  # 'forgot' to draw from the strategy
             pass
+
+
+def test_adds_note_showing_which_strategy():
+    class X:
+        def __init__(self, y: int) -> None:
+            assert y == 7
+
+    @given(...)
+    def inner(value: X):
+        assert isinstance(value, X)
+
+    rep = re.escape(repr(st.from_type(X)))
+    with pytest.raises(AssertionError, match=f".*while generating 'value' from {rep}"):
+        inner()
+
+
+def test_adds_note_showing_which_strategy_stateful():
+    strategy = st.integers().map(lambda x: x / 0)
+
+    class Machine(RuleBasedStateMachine):
+        @rule(value=strategy)
+        def take_a_step(self, value):
+            assert value
+
+    msg = f"while generating 'value' from {strategy!r} for rule take_a_step"
+    print(msg)
+    with pytest.raises(ZeroDivisionError, match=f".*{re.escape(msg)}"):
+        run_state_machine_as_test(Machine)
