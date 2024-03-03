f696fa947013b1f1aa2adbefd18e448434ed4b99
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2024-03-03 08:14:05 +0100

    Fix numpy extra install in whole repo tests

--- tooling/src/hypothesistooling/__main__.py
+++ tooling/src/hypothesistooling/__main__.py
@@ -519,7 +519,14 @@ def check_examples3(*args):
 def check_whole_repo_tests(*args):
     install.ensure_shellcheck()
     subprocess.check_call(
-        [sys.executable, "-m", "pip", "install", "--upgrade", hp.HYPOTHESIS_PYTHON+"[numpy]"]
+        [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+            "--upgrade",
+            str(hp.HYPOTHESIS_PYTHON) + "[numpy]",
+        ]
     )
 
     if not args:
