9bed94118083359ecde80f60c57972cdbbcdf50a
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2024-03-09 13:37:07 +0100

    Improve and test hypothesis.extra.numpy type annotations

---
+++ hypothesis-python/RELEASE.rst
@@ -0,0 +1,3 @@
+RELEASE_TYPE: patch
+
+This patch further improves the type annotations in :mod:`hypothesis.extra.numpy`.
--- whole-repo-tests/test_mypy.py
+++ whole-repo-tests/test_mypy.py
@@ -131,6 +131,118 @@ def assert_mypy_errors(fname, expected, python_version=None):
             'arrays(dtype=np.dtype("int32"), shape=1)',
             "ndarray[Any, dtype[signedinteger[_32Bit]]]",
         ),
+        (
+            "arrays(dtype=np.dtype(int), shape=1)",
+            "ndarray[Any, dtype[Any]]",
+        ),
+        (
+            "boolean_dtypes()",
+            "dtype[bool_]",
+        ),
+        (
+            "unsigned_integer_dtypes(sizes=8)",
+            "dtype[unsignedinteger[_8Bit]]",
+        ),
+        (
+            "unsigned_integer_dtypes(sizes=16)",
+            "dtype[unsignedinteger[_16Bit]]",
+        ),
+        (
+            "unsigned_integer_dtypes(sizes=32)",
+            "dtype[unsignedinteger[_32Bit]]",
+        ),
+        (
+            "unsigned_integer_dtypes(sizes=64)",
+            "dtype[unsignedinteger[_64Bit]]",
+        ),
+        (
+            "unsigned_integer_dtypes()",
+            "dtype[unsignedinteger[Any]]",
+        ),
+        (
+            "unsigned_integer_dtypes(sizes=(8, 16))",
+            "dtype[unsignedinteger[Any]]",
+        ),
+        (
+            "integer_dtypes(sizes=8)",
+            "dtype[signedinteger[_8Bit]]",
+        ),
+        (
+            "integer_dtypes(sizes=16)",
+            "dtype[signedinteger[_16Bit]]",
+        ),
+        (
+            "integer_dtypes(sizes=32)",
+            "dtype[signedinteger[_32Bit]]",
+        ),
+        (
+            "integer_dtypes(sizes=64)",
+            "dtype[signedinteger[_64Bit]]",
+        ),
+        (
+            "integer_dtypes()",
+            "dtype[signedinteger[Any]]",
+        ),
+        (
+            "integer_dtypes(sizes=(8, 16))",
+            "dtype[signedinteger[Any]]",
+        ),
+        (
+            "floating_dtypes(sizes=16)",
+            "dtype[floating[_16Bit]]",
+        ),
+        (
+            "floating_dtypes(sizes=32)",
+            "dtype[floating[_32Bit]]",
+        ),
+        (
+            "floating_dtypes(sizes=64)",
+            "dtype[floating[_64Bit]]",
+        ),
+        (
+            "floating_dtypes(sizes=128)",
+            "dtype[floating[_128Bit]]",
+        ),
+        (
+            "floating_dtypes()",
+            "dtype[floating[Any]]",
+        ),
+        (
+            "floating_dtypes(sizes=(16, 32))",
+            "dtype[floating[Any]]",
+        ),
+        (
+            "complex_number_dtypes(sizes=64)",
+            "dtype[complexfloating[_32Bit, _32Bit]]",
+        ),
+        (
+            "complex_number_dtypes(sizes=128)",
+            "dtype[complexfloating[_64Bit, _64Bit]]",
+        ),
+        (
+            "complex_number_dtypes(sizes=256)",
+            "dtype[complexfloating[_128Bit, _128Bit]]",
+        ),
+        (
+            "complex_number_dtypes()",
+            "dtype[complexfloating[Any, Any]]",
+        ),
+        (
+            "complex_number_dtypes(sizes=(64, 128))",
+            "dtype[complexfloating[Any, Any]]",
+        ),
+        (
+            "integer_array_indices(shape=(2, 3))",
+            "Tuple[ndarray[Any, dtype[signedinteger[Any]]",
+        ),
+        (
+            'integer_array_indices(shape=(2, 3), dtype=np.dtype("int32"))',
+            "Tuple[ndarray[Any, dtype[signedinteger[_32Bit]]",
+        ),
+        (
+            'integer_array_indices(shape=(2, 3), dtype=np.dtype("uint8"))',
+            "Tuple[ndarray[Any, dtype[unsignedinteger[_8Bit]]",
+        ),
         # Note: keep this in sync with the equivalent test for Pyright
     ],
 )
