5c57fe0b2f8d93a04524fed7aa9049cb28a08f13
Jonathan Plasse <13716151+JonathanPlasse@users.noreply.github.com>
2024-03-10 08:30:23 +0100

    Separate Numpy revealed types tests

--- whole-repo-tests/test_pyright.py
+++ whole-repo-tests/test_pyright.py
@@ -23,6 +23,8 @@ import pytest
 from hypothesistooling.projects.hypothesispython import HYPOTHESIS_PYTHON, PYTHON_SRC
 from hypothesistooling.scripts import pip_tool, tool_path
 
+from .revealed_types import NUMPY_REVEALED_TYPES
+
 PYTHON_VERSIONS = ["3.7", "3.8", "3.9", "3.10", "3.11"]
 
 
@@ -207,122 +209,6 @@ def test_numpy_arrays_strategy(tmp_path: Path):
             "tuples(text(), text(), text(), text(), text(), text())",
             "Tuple[Any, ...]",
         ),
-        (
-            'arrays(dtype=np.dtype("int32"), shape=1)',
-            "ndarray[Any, dtype[signedinteger[_32Bit]]]",
-        ),
-        (
-            "arrays(dtype=np.dtype(int), shape=1)",
-            "ndarray[Any, dtype[signedinteger[Any]]]",
-        ),
-        (
-            "boolean_dtypes()",
-            "dtype[bool_]",
-        ),
-        (
-            "unsigned_integer_dtypes(sizes=8)",
-            "dtype[unsignedinteger[_8Bit]]",
-        ),
-        (
-            "unsigned_integer_dtypes(sizes=16)",
-            "dtype[unsignedinteger[_16Bit]]",
-        ),
-        (
-            "unsigned_integer_dtypes(sizes=32)",
-            "dtype[unsignedinteger[_32Bit]]",
-        ),
-        (
-            "unsigned_integer_dtypes(sizes=64)",
-            "dtype[unsignedinteger[_64Bit]]",
-        ),
-        (
-            "unsigned_integer_dtypes()",
-            "dtype[unsignedinteger[Any]]",
-        ),
-        (
-            "unsigned_integer_dtypes(sizes=(8, 16))",
-            "dtype[unsignedinteger[Any]]",
-        ),
-        (
-            "integer_dtypes(sizes=8)",
-            "dtype[signedinteger[_8Bit]]",
-        ),
-        (
-            "integer_dtypes(sizes=16)",
-            "dtype[signedinteger[_16Bit]]",
-        ),
-        (
-            "integer_dtypes(sizes=32)",
-            "dtype[signedinteger[_32Bit]]",
-        ),
-        (
-            "integer_dtypes(sizes=64)",
-            "dtype[signedinteger[_64Bit]]",
-        ),
-        (
-            "integer_dtypes()",
-            "dtype[signedinteger[Any]]",
-        ),
-        (
-            "integer_dtypes(sizes=(8, 16))",
-            "dtype[signedinteger[Any]]",
-        ),
-        (
-            "floating_dtypes(sizes=16)",
-            "dtype[floating[_16Bit]]",
-        ),
-        (
-            "floating_dtypes(sizes=32)",
-            "dtype[floating[_32Bit]]",
-        ),
-        (
-            "floating_dtypes(sizes=64)",
-            "dtype[floating[_64Bit]]",
-        ),
-        (
-            "floating_dtypes(sizes=128)",
-            "dtype[floating[_128Bit]]",
-        ),
-        (
-            "floating_dtypes()",
-            "dtype[floating[Any]]",
-        ),
-        (
-            "floating_dtypes(sizes=(16, 32))",
-            "dtype[floating[Any]]",
-        ),
-        (
-            "complex_number_dtypes(sizes=64)",
-            "dtype[complexfloating[_32Bit, _32Bit]]",
-        ),
-        (
-            "complex_number_dtypes(sizes=128)",
-            "dtype[complexfloating[_64Bit, _64Bit]]",
-        ),
-        (
-            "complex_number_dtypes(sizes=256)",
-            "dtype[complexfloating[_128Bit, _128Bit]]",
-        ),
-        (
-            "complex_number_dtypes()",
-            "dtype[complexfloating[Any, Any]]",
-        ),
-        (
-            "complex_number_dtypes(sizes=(64, 128))",
-            "dtype[complexfloating[Any, Any]]",
-        ),
-        (
-            "integer_array_indices(shape=(2, 3))",
-            "Tuple[ndarray[Any, dtype[signedinteger[Any]]], ...]",
-        ),
-        (
-            'integer_array_indices(shape=(2, 3), dtype=np.dtype("int32"))',
-            "Tuple[ndarray[Any, dtype[signedinteger[_32Bit]]], ...]",
-        ),
-        (
-            'integer_array_indices(shape=(2, 3), dtype=np.dtype("uint8"))',
-            "Tuple[ndarray[Any, dtype[unsignedinteger[_8Bit]]], ...]",
-        ),
         # Note: keep this in sync with the equivalent test for Mypy
     ],
 )
@@ -330,10 +216,30 @@ def test_revealed_types(tmp_path, val, expect):
     """Check that Pyright picks up the expected `X` in SearchStrategy[`X`]."""
     f = tmp_path / (expect + ".py")
     f.write_text(
-        "import numpy as np\n"
-        "from hypothesis.extra.numpy import *\n"
-        "from hypothesis.strategies import *\n"
-        f"reveal_type({val})\n",  # fmt: skip
+        textwrap.dedent(
+            f"""
+            from hypothesis.strategies import *
+            reveal_type({val})
+            """
+        ),
+        encoding="utf-8",
+    )
+    _write_config(tmp_path, {"reportWildcardImportFromLibrary ": "none"})
+    typ = get_pyright_analysed_type(f)
+    assert typ == f"SearchStrategy[{expect}]"
+
+
+@pytest.mark.parametrize("val,expect", NUMPY_REVEALED_TYPES)
+def test_numpy_revealed_types(tmp_path, val, expect):
+    f = tmp_path / (expect + ".py")
+    f.write_text(
+        textwrap.dedent(
+            f"""
+            import numpy as np
+            from hypothesis.extra.numpy import *
+            reveal_type({val})
+            """
+        ),
         encoding="utf-8",
     )
     _write_config(tmp_path, {"reportWildcardImportFromLibrary ": "none"})
