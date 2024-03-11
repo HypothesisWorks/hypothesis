cbdedfe5d9590855f1e60a50b2e470454aaa976d
Zac Hatfield-Dodds <zac.hatfield.dodds@gmail.com>
2024-03-10 19:43:55 -0700

    allow cross-imports in whole-repo-tests

--- whole_repo_tests/revealed_types.py
+++ whole_repo_tests/revealed_types.py
@@ -1,3 +1,13 @@
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis/
+#
+# Copyright the Hypothesis Authors.
+# Individual contributors are listed in AUTHORS.rst and the git log.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
 NUMPY_REVEALED_TYPES = [
     (
         'arrays(dtype=np.dtype("int32"), shape=1)',
