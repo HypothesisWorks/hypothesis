6a32fb3a048924f409dd5ed9899c3f9aeb6d9b48
AathiraTD <123739268+AathiraTD@users.noreply.github.com>
2024-05-19 22:58:35 +0100

    Update example.py
    Additio nof 2 numbers

--- example.py
+++ example.py
@@ -1,3 +1,14 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
 def greet():
 print('Hello, World!')
 print('Hello from GitHub!')
