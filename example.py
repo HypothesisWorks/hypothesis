c91d3abd777cbfa40d1d5b0c03718306edf8eb21
AathiraTD <123739268+AathiraTD@users.noreply.github.com>
2024-05-19 23:00:46 +0100

    Leap year Check

--- example.py
+++ example.py
@@ -1,3 +1,25 @@
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
+
 def greet():
 print('Hello, World!')
 print('Hello from GitHub!')
