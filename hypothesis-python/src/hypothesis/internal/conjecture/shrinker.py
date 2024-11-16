10f6c2c2fc2dfb70ca92f98801619677193431e2
Liam DeVoe <orionldevoe@gmail.com>
2024-11-16 00:39:13 -0500

    no cover incorporate_new_buffer

--- hypothesis-python/src/hypothesis/internal/conjecture/shrinker.py
+++ hypothesis-python/src/hypothesis/internal/conjecture/shrinker.py
@@ -413,7 +413,7 @@ class Shrinker:
         buffer = bytes(buffer)
         return buffer.startswith(self.buffer) or self.incorporate_new_buffer(buffer)
 
-    def incorporate_new_buffer(self, buffer):
+    def incorporate_new_buffer(self, buffer):  # pragma: no cover
         """Either runs the test function on this buffer and returns True if
         that changed the shrink_target, or determines that doing so would
         be useless and returns False without running it."""
