ece9f7b9e2b3d9dee52aea10b5c126a62603e39c
Liam DeVoe <orionldevoe@gmail.com>
2025-01-10 14:55:23 -0500

    dont print symbolics in verbose mode

---
+++ hypothesis-python/tests/crosshair/test_crosshair.py
@@ -0,0 +1,26 @@
+# This file is part of Hypothesis, which may be found at
+# https://github.com/HypothesisWorks/hypothesis/
+#
+# Copyright the Hypothesis Authors.
+# Individual contributors are listed in AUTHORS.rst and the git log.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
+import pytest
+
+from hypothesis import Verbosity, given, settings, strategies as st
+
+
+@pytest.mark.parametrize("verbosity", list(Verbosity))
+def test_crosshair_works_for_all_verbosities(verbosity):
+    # check that we aren't realizing symbolics early in debug prints and killing
+    # test effectiveness.
+    @given(st.integers())
+    @settings(backend="crosshair", verbosity=verbosity, database=None)
+    def f(n):
+        assert n != 123456
+
+    with pytest.raises(AssertionError):
+        f()
