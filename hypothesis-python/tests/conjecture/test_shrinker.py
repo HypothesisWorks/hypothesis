c9c9464c1d1532142b742129c74f3ac425d5ca20
Liam DeVoe <orionldevoe@gmail.com>
2025-03-14 19:02:42 -0400

    improve one_of shrinking coverage case

--- hypothesis-python/tests/conjecture/test_shrinker.py
+++ hypothesis-python/tests/conjecture/test_shrinker.py
@@ -26,7 +26,6 @@ from hypothesis.internal.conjecture.shrinking.common import Shrinker as Shrinker
 from hypothesis.internal.conjecture.utils import Sampler
 from hypothesis.internal.floats import MAX_PRECISE_INTEGER
 
-from tests.common.debug import minimal
 from tests.conjecture.common import (
     SOME_LABEL,
     float_kw,
@@ -661,10 +660,12 @@ def test_lower_duplicated_characters_across_choices(start, expected, gap):
 def test_shrinking_one_of_with_same_shape():
     # This is a covering test for our one_of shrinking logic for the case when
     # the choice sequence *doesn't* change shape in the newly chosen one_of branch.
-    #
-    # There are relatively few tests in our suite that cover this (and previously
-    # none in the covering subset). I chose the simplest one to copy here, but
-    # haven't yet put time into extracting the essence of a test case that
-    # covers this case, which is why we're using st.permutations here instead of
-    # something more fundamental / obviously testing what we want.
-    minimal(st.permutations(list(range(5))), lambda x: x[0] != 0)
+    @shrinking_from([1, 0])
+    def shrinker(data: ConjectureData):
+        n = data.draw_integer(0, 1)
+        data.draw_integer()
+        if n == 1:
+            data.mark_interesting()
+
+    shrinker.initial_coarse_reduction()
+    assert shrinker.choices == (1, 0)
