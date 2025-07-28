49c356eaf375af4935ca4956be24034cf5b76ef4
Liam DeVoe <orionldevoe@gmail.com>
2025-07-28 05:26:45 +0000

    reword health check messages

--- hypothesis-python/src/hypothesis/errors.py
+++ hypothesis-python/src/hypothesis/errors.py
@@ -168,7 +168,7 @@ class HypothesisWarning(HypothesisException, Warning):
 
 
 class FailedHealthCheck(_Trimmable):
-    """Raised when a test fails a healthcheck."""
+    """Raised when a test fails a health check. See |HealthCheck|."""
 
 
 class NonInteractiveExampleWarning(HypothesisWarning):
