a48581f932a382eef00f405b72c6fdc8e3c1ecfd
Liam DeVoe <orionldevoe@gmail.com>
2026-01-04 03:05:44 -0500

    better TypeAliasType resolution

--- hypothesis-python/tests/cover/test_typealias_py312.py
+++ hypothesis-python/tests/cover/test_typealias_py312.py
@@ -8,10 +8,14 @@
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at https://mozilla.org/MPL/2.0/.
 
+from collections.abc import Callable
+from typing import get_args, get_origin
+
 import pytest
 
 from hypothesis import strategies as st
-from hypothesis.errors import InvalidArgument
+from hypothesis.errors import HypothesisException
+from hypothesis.strategies._internal.types import evaluate_type_alias_type
 
 from tests.common.debug import assert_simple_property, find_any
 from tests.common.utils import temp_registered
@@ -40,6 +44,13 @@ def test_resolves_nested():
     assert_simple_property(st.from_type(Point3), lambda x: isinstance(x, int))
 
 
+def test_resolves_parametrized():
+    type MyList = list[int]
+    assert_simple_property(
+        st.from_type(MyList), lambda l: all(isinstance(x, int) for x in l)
+    )
+
+
 def test_mutually_recursive_fails():
     # example from
     # https://docs.python.org/3/library/typing.html#typing.TypeAliasType.__value__
@@ -50,6 +61,15 @@ def test_mutually_recursive_fails():
     # is...complicated.
     with pytest.raises(RecursionError):
         find_any(st.from_type(A))
+
+
+def test_mutually_recursive_fails_parametrized():
+    # same with parametrized types
+    type A[T] = B[T]
+    type B[T] = A[T]
+
+    with pytest.raises(RecursionError):
+        find_any(st.from_type(A[int]))
 
 
 def test_can_register_typealias():
@@ -79,12 +99,12 @@ def test_resolves_parameterized_typealias():
 
 
 def test_resolves_nested_parameterized_typealias():
-    type Outer[T] = list[T]
-    type Inner[T] = Outer[T]
+    type Inner[T] = list[T]
+    type Outer[T] = Inner[T]
 
-    assert_simple_property(st.from_type(Inner[str]), lambda x: isinstance(x, list))
+    assert_simple_property(st.from_type(Outer[str]), lambda x: isinstance(x, list))
     assert_simple_property(
-        st.from_type(Inner[str]), lambda x: all(isinstance(i, str) for i in x)
+        st.from_type(Outer[str]), lambda x: all(isinstance(i, str) for i in x)
     )
 
 
@@ -99,28 +119,13 @@ def test_resolves_parameterized_typealias_with_literal_types():
     )
 
 
-def test_parameterized_typealias_with_unused_params_errors():
-    # Type aliases with unused type parameters are ambiguous
-    type MyList[T1, T2] = list[T1]
-
-    with pytest.raises(
-        InvalidArgument,
-        match=r"Cannot resolve.*MyList.*declares 2 type parameter.*only uses 1",
-    ):
-        # Error is raised during strategy validation
-        find_any(st.from_type(MyList[int, float]))
-
-
 def test_can_register_parameterized_typealias_with_unused_params():
     # Users can explicitly register strategies for such types using a resolver function
     type MyList[T1, T2] = list[T1]
 
     # Register a function that resolves the type alias
     def resolve_mylist(thing):
-        from typing import get_args, get_origin
-
-        origin = get_origin(thing)
-        if origin is MyList:
+        if get_origin(thing) is MyList:
             args = get_args(thing)
             # Use the first type argument, ignore the second
             return st.lists(st.from_type(args[0]))
@@ -134,3 +139,41 @@ def test_can_register_parameterized_typealias_with_unused_params():
     assert_simple_property(
         st.from_type(MyList[int, float]), lambda x: all(isinstance(i, int) for i in x)
     )
+
+
+def test_typealias_evaluation():
+    type A[T1, T2] = list[T1]
+    assert evaluate_type_alias_type(A[int, float]) == list[int]
+
+    type A[T1, T2] = list[T2]
+    assert evaluate_type_alias_type(A[float, int]) == list[int]
+
+    type A[K, V] = dict[V, K]
+    assert evaluate_type_alias_type(A[str, int]) == dict[int, str]
+
+    type A[T] = list[list[T]]
+    assert evaluate_type_alias_type(A[int]) == list[list[int]]
+
+    type Inner[T] = list[T]
+    type Outer[T] = Inner[T]
+    assert evaluate_type_alias_type(Outer[int]) == list[int]
+
+    type Bare[T] = list
+    assert evaluate_type_alias_type(Bare[int]) == list
+
+    type A[T1, T2] = list[T1]
+    assert evaluate_type_alias_type(A[int]) == list[int]
+
+    # tries to reference free variable
+    type A[T1, T2] = list[T2]
+    with pytest.raises(ValueError):
+        evaluate_type_alias_type(A[int])
+
+    # (currently) unsupported type forms
+    type A[*Ts] = tuple[*Ts]
+    with pytest.raises(HypothesisException, match="Hypothesis does not yet support"):
+        assert evaluate_type_alias_type(A[int, str, float]) == tuple[int, str, float]
+
+    type A[**P] = Callable[P, int]
+    with pytest.raises(HypothesisException, match="Hypothesis does not yet support"):
+        assert evaluate_type_alias_type(A[[str, float]]) == Callable[[str, float], int]
